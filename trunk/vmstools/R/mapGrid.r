94255c09f29064ddb8ceb7c451a9b546086d355d
southandy <southandy@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-02-15 20:33:18 +0000

    created package structure, added vmsGridCreate.r and mapGrid.r

---
+++ trunk/vmstools/R/mapGrid.r
@@ -0,0 +1,54 @@
+#mapGrid.r
+#andy south 12/2/09
+
+#to map grids input as SGDF
+#** OR  maybe just provide a wrapper to mapGriddedData instead ?
+#would need to add a better worldmap into that
+
+mapGrid <- function( sGDF
+                         , sPDF
+                         , we=""
+                         , ea=""
+                         , so=""
+                         , no=""
+                         , gridValName="fishing"
+                         , plotTitle = ""
+                         , numCats = 5
+                         , addLegend = TRUE
+                         , legendx='bottomleft'
+                         , legendncol = 1
+                         , legendtitle = "fishing activity"
+                         , plotPoints = FALSE
+                         , outGridFile = ""  #name for output gridAscii
+                         , outPlot = ""  #name for output png
+                         , ... )
+{
+
+require(sp)
+require(maptools)
+
+
+xlim=c(we,ea)
+ylim=c(so,no)
+
+#dev.new()
+breaks <- pretty(sGDF[[gridValName]],n=numCats)
+cols <- rev(heat.colors((length(breaks)-1)))
+image(sGDF, attr=gridValName, axes=FALSE, breaks = breaks, col=cols, xlim=xlim, ylim=ylim   )
+library(mapdata)
+map("worldHires", add=TRUE)
+box() # to put a box around the plot
+#mtext(paste(gear,year),font=4,line=-1.5)
+
+#legend(x='bottomleft', legend=breaks[1:(length(breaks)-1)], pch = 22, pt.bg=cols, title="fishing activity",bg="white",pt.cex=2 )
+legend(x=legendx, legend=breaks[1:(length(breaks)-1)], pch = 22, pt.bg=cols, title=legendtitle, ncol=legendncol, bg="white",pt.cex=2 )
+
+#to add points (can obscure grid)
+if (plotPoints) points(sPDF, pch=".")
+
+#to add plotTitle
+if (plotTitle != "") mtext(plotTitle)
+
+
+
+} #end of mapGrid
