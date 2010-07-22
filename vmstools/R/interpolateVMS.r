cbf655a379ff2d6f5565605422534633d3d169d7
Niels.Hintzen <niels.hintzen@gmail.com>
2010-07-22 15:13:17 +0000

    changed reduction in lon direction of vector into reduction in lat direction to make vector length equal in km effect

--- vmstools/R/interpolateVMS.r
+++ vmstools/R/interpolateVMS.r
@@ -73,10 +73,10 @@ for(iStep in 1:(dim(VMS.)[1]-1)){
       My1 <- VMS.[endVMS,   "declat"]
 
         #Corrected for longitude lattitude effect
-      Hx0 <- Hx0 * params$fm * lonLatRatio(VMS.[c(startVMS,endVMS),"declon"],VMS.[c(startVMS,endVMS),"declat"])[1]
-      Hx1 <- Hx1 * params$fm * lonLatRatio(VMS.[c(startVMS,endVMS),"declon"],VMS.[c(startVMS,endVMS),"declat"])[2]
-      Hy0 <- Hy0 * params$fm
-      Hy1 <- Hy1 * params$fm
+      Hx0 <- Hx0 * params$fm 
+      Hx1 <- Hx1 * params$fm 
+      Hy0 <- Hy0 * params$fm * lonLatRatio(VMS.[c(startVMS,endVMS),"declon"],VMS.[c(startVMS,endVMS),"declat"])[1]
+      Hy1 <- Hy1 * params$fm * lonLatRatio(VMS.[c(startVMS,endVMS),"declon"],VMS.[c(startVMS,endVMS),"declat"])[2]
 
         #Finalizing the interpolation based on cHs
       fx <- numeric()
