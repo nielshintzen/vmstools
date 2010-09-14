88168683f67825eacd63f9a3eb7575fb46908224
doug.beare <doug.beare@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-09-14 12:44:04 +0000

    Changed datim to DATIM and checked with unmerged and merged tacsat data.

--- vmstools/R/intervalTacsat.r
+++ vmstools/R/intervalTacsat.r
@@ -8,7 +8,7 @@ intervalTacsat <- function(tacsat,level="trip"){
                                                             return(unlist(res))}))
                     }
                     if(level=="vessel"){
-                      tacsat$timeint[2:nrow(tacsat)]  <- difftime(tacsat$datim[2:nrow(tacsat)],tacsat$datim[1:(nrow(tacsat)-1)])
+                      tacsat$timeint[2:nrow(tacsat)]  <- difftime(tacsat$DATIM[2:nrow(tacsat)],tacsat$DATIM[1:(nrow(tacsat)-1)])
                       vessels                         <- unique(tacsat$VE_REF)
                       first.vessels                   <- unlist(lapply(as.list(vessels),function(x){which(tacsat$VE_REF==x)[1]}))
                       tacsat$timeint[first.vessels-1] <- NA
