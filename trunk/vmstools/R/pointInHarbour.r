a068576e566b014cc109eccdf3ca76e3a46dad4d
niels.hintzen@gmail.com <niels.hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-09-17 09:44:42 +0000

    speed-up of distance.r by 20% speed-up of 50x in pointInHarbour by using chunks of data and smart harbour selection

--- trunk/vmstools/R/pointInHarbour.r
+++ trunk/vmstools/R/pointInHarbour.r
@@ -1,16 +1,56 @@
-pointInHarbour <- function(lon,lat,harbours){
+pointInHarbour <- function(lon,lat,harbours,rowSize=30){
 
-    inHarbour   <- numeric()
-    for(hars in 1:length(harbours)){
-      #print(hars)
-      lonDegree <- 0.1*(harbours[[hars]]$range/(11.2*lonLatRatio(lon=harbours[[hars]]$lon,lat=harbours[[hars]]$lat)[1]))
-      latDegree <- 0.1*(harbours[[hars]]$range/11.2)
+    xharb     <- unlist(lapply(harbours,function(x){return(x[,"lon"])}))
+    yharb     <- unlist(lapply(harbours,function(x){return(x[,"lat"])}))
+    rharb     <- unlist(lapply(harbours,function(x){return(x[,"range"])}))
+    harb      <- cbind(xharb,yharb,rharb)
+    harb      <- orderBy(~xharb+yharb,data=harb)
 
-      #WHY IS LEVEL ~0.392??
-      harPolygon <- ellipse(x=0,scale=c(lonDegree,latDegree),centre=c(harbours[[hars]]$lon,harbours[[hars]]$lat),level=0.392)
-      res        <- point.in.polygon(point.x=lon,point.y=lat,pol.x=harPolygon[,"x"],pol.y=harPolygon[,"y"])
-      inHarbour  <- unique(c(inHarbour,which(res == 1)))
+    nChunks   <- ceiling(length(lon)/rowSize)
+    store     <- numeric(length(lon))
+    for(chunks in 1:nChunks){
+      if(chunks == nChunks){
+        x1    <- lon[(chunks*rowSize-rowSize+1):length(lon)]
+        y1    <- lat[(chunks*rowSize-rowSize+1):length(lon)]
+      } else {
+          x1    <- lon[(chunks*rowSize-rowSize+1):(chunks*rowSize)]
+          y1    <- lat[(chunks*rowSize-rowSize+1):(chunks*rowSize)]
+        }
+
+      xr        <- range(x1,na.rm=T); xr <- c(xr[1]-0.05,xr[2]+0.05)
+      yr        <- range(y1,na.rm=T); yr <- c(yr[1]-0.05,yr[2]+0.05)
+      res1      <- which(harb[,"xharb"] >= xr[1] & harb[,"xharb"] <= xr[2])
+      res2      <- which(harb[,"yharb"] >= yr[1] & harb[,"yharb"] <= yr[2])
+      res3      <- res1[which(is.na(pmatch(res1,res2))==F)]
+
+      if(length(res3)>0){
+        for(hars in res3){
+          #print(hars)
+          x2  <- harb[hars,"xharb"]
+          y2  <- harb[hars,"yharb"]
+
+          pd  <- pi/180
+
+          a1  <- sin(((y2-y1)*pd)/2)
+          a2  <- cos(y1*pd)
+          a3  <- cos(y2*pd)
+          a4  <- sin(((x2-x1)*pd)/2)
+          a   <- a1*a1+a2*a3*a4*a4
+
+          c   <- 2*atan2(sqrt(a),sqrt(1-a));
+          R   <- 6371;
+          dx1 <- R*c
+
+          res <- numeric(length(x1))
+          res[which(dx1<=harb[hars,"rharb"])] <- 1
+
+          if(chunks==nChunks){ store[(chunks*rowSize-rowSize+1):length(lon)]  <- pmax(store[(chunks*rowSize-rowSize+1):length(lon)],res,na.rm=T)
+          } else { store[(chunks*rowSize-rowSize+1):(chunks*rowSize)]         <- pmax(store[(chunks*rowSize-rowSize+1):(chunks*rowSize)],res,na.rm=T)}
+        }
+      }
     }
 
-return(inHarbour)}
+return(store)}
     
+
+
