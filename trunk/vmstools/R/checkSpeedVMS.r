e329b0a5b575bc76e94e1e8a9afa0aaecb14c2b9
doug.beare <doug.beare@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-09-23 11:54:37 +0000

    Changed this so it should work with tacsat... needs checking... and improving since it is slow.........

--- trunk/vmstools/R/checkSpeedVMS.r
+++ trunk/vmstools/R/checkSpeedVMS.r
@@ -1,20 +1,23 @@
-checkSpeedVMS <- function(VMS){
+checkSpeedVMS <- function(tacsat=tacsat){
 
-for(i in 1:(dim(VMS)[1]-1)){
-  if(i==1) ship <- VMS$ship[1]
-  if(ship == VMS$ship[i+1]){
-    dstn  <- degree2Km(VMS$declon[i],VMS$declat[i],distance(VMS$declon[i+1],VMS$declat[i+1],VMS$declon[i],VMS$declat[i]))
-    time  <- an(difftime(VMS$date[i+1],VMS$date[i],units=c("mins")))
-    spd   <- mean(VMS$speed[i+1],VMS$speed[i],na.rm=T)
+tacsat$SI_DATIM  <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M:%S")
+                          
+
+for(i in 1:(dim(tacsat)[1]-1)){
+  if(i==1) VE_REF <- tacsat$VE_REF[1]
+  if(VE_REF == tacsat$VE_REF[i+1]){
+    dstn  <- degree2Km(tacsat$SI_LONG[i],tacsat$SI_LATI[i],distance(tacsat$SI_LONG[i+1],tacsat$SI_LATI[i+1],tacsat$SI_LONG[i],tacsat$SI_LATI[i]))
+    time  <- an(difftime(tacsat$SI_DATIM[i+1],tacsat$SI_DATIM[i],units=c("mins")))
+    spd   <- mean(tacsat$SI_SP[i+1],tacsat$SI_SP[i],na.rm=T)
     
     maxdist <- spd*1.852*time/120
     if(dstn > maxdist){
       warning("Speed to low to get at other point")
-      VMS$speed[i] <- NA
+      tacsat$SI_SP[i] <- NA
     }
   } else {
-      ship <- VMS$ship[i+1]
+      VE_REF <- tacsat$VE_REF[i+1]
     }
 }
-return(VMS)}
+return(tacsat)}
     
