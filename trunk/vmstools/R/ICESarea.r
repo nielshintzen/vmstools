cd37b4c98b3d04b066f233b5170e3d94ef58f9a6
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-10-12 14:37:10 +0000

    assign a ICES area to each ping...need the shape file from ICES

---
+++ trunk/vmstools/R/ICESarea.r
@@ -0,0 +1,17 @@
+ # function to assign a ICES area code to each ping
+ # from the ICES_areas shape file
+ ICESarea <- function(long, lat,
+       shape.file=file.path("C:","ICES_areas")){
+   library(shapefiles)
+   library(sp)
+   an <- function(x) as.numeric(as.character(x))
+   ICES_area <- rep(NA, length(long))  # init
+   a.shape <- read.shapefile(shape.file)
+   for (i in 1:length(a.shape$shp$shp)){
+     print(a.shape$dbf$ICESAREA[i])
+     a.polygon <- cbind(a.shape$shp$shp[[i]]$points$X,a.shape$shp$shp[[i]]$points$Y)
+     idx.inout <- point.in.polygon(an(long), an(lat), a.polygon[,1], a.polygon[,2])
+     ICES_area[idx.inout==1] <- as.character(a.shape$dbf$dbf$ICESAREA[i])
+     }
+ return(ICES_area)
+ }
