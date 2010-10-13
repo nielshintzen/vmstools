8249aede72807c6da65f0f245232584035be05fd
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-10-13 12:24:16 +0000

    modified to be more generic

--- trunk/vmstools/R/ICESarea.r
+++ trunk/vmstools/R/ICESarea.r
@@ -1,18 +1,19 @@
-# function to assign a ICES area code to each ping 
+ # function to assign a ICES area code to each ping
  # from the ICES_areas shape file
- ICESarea <- function(long, lat,
-       shape.file=file.path("C:","ICES_areas")){
-   library(shapefiles) 
+ ICESarea <- function(long, lat,...){
+   lstargs <- list(...)
+   library(shapefiles)
    library(sp)
+   if(length(lstargs$shape.file)==0) { data(ICESareas)
+   } else { ICESareas <- read.shapefile(lstargs$shape.file) }
    an <- function(x) as.numeric(as.character(x))
    ICES_area <- rep(NA, length(long))  # init
-   a.shape <- read.shapefile(shape.file)  
-   for (i in 1:length(a.shape$shp$shp)){       
-     print(as.character(a.shape$dbf$dbf$ICESAREA[i]))
-     a.polygon <- cbind(a.shape$shp$shp[[i]]$points$X,a.shape$shp$shp[[i]]$points$Y)  
-     idx.inout <- point.in.polygon(an(long), an(lat), a.polygon[,1], a.polygon[,2]) 
-     ICES_area[idx.inout==1] <- as.character(a.shape$dbf$dbf$ICESAREA[i])
-     }           
+   for (i in 1:length(ICESareas$shp$shp)){
+     print(as.character(ICESareas$dbf$dbf$ICESAREA[i]))
+     a.polygon <- cbind(ICESareas$shp$shp[[i]]$points$X, ICESareas$shp$shp[[i]]$points$Y)
+     idx.inout <- point.in.polygon(an(long), an(lat), a.polygon[,1], a.polygon[,2])
+     ICES_area[idx.inout==1] <- as.character(ICESareas$dbf$dbf$ICESAREA[i])
+     }
  return(ICES_area)
  }
  #all.merged$ICES_area <- ICESarea(long= all.merged$SI_LONG, lat= all.merged$SI_LATI,
