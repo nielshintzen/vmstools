1dff22b524e4e68b260f2f3c0ead9e7de834bc61
doug.beare <doug.beare@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-10-19 14:03:57 +0000

    Simple function to divide landings among pings. Does similar things to Francois' program but far less sophisticated.

---
+++ trunk/vmstools/R/SplitLandingsAmongPings.r
@@ -0,0 +1,28 @@
+
+SplitLandingsAmongPings <- function(tacsat=tacsat, eflalo2=eflalo2, which.species="COD")
+{
+#This function uses the output from count pings to distribute the catches of ONE chosen species recorded in eflalo over the pings in tacsat.
+#It then adds on the total weight and total landing values of all species in eflalo2
+dn <- dimnames(eflalo2)[[2]]
+yp <- NULL
+for(ss in which.species){yp <- c(yp,grep(ss,dn)) }
+kgs <- eflalo2[,yp[1]][match(tacsat$FT_REF,eflalo2$FT_REF)]
+tacsat$kgs.by.ping <- kgs/tacsat$npings
+cash <- eflalo2[,yp[2]][match(tacsat$FT_REF,eflalo2$FT_REF)]
+tacsat$cash.by.ping <- cash/tacsat$npings
+#All species
+#KGS
+yp <- grep("_KG_",dn)
+eflalo2$kgs <- apply(eflalo2[,yp],1,sum,na.rm=T)
+tacsat$total.kgs <- eflalo2$kgs[ match(tacsat$FT_REF,eflalo2$FT_REF)]
+#VALUES
+yp <- grep("_EURO_",dn)
+eflalo2$cash <- apply(eflalo2[,yp],1,sum,na.rm=T)
+tacsat$total.cash <- eflalo2$cash[ match(tacsat$FT_REF,eflalo2$FT_REF)]
+tacsat
+
+}
+
+#Example
+
+#tacsat1f <- SplitLandingsAmongPings(tacsat=tacsat1f,eflalo2=eflalo2,which.species="COD")
