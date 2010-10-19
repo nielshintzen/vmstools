270f3350e100867d16555e9598ad41fa8bff4978
nicolas.deporte <nicolas.deporte@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-10-19 13:58:08 +0000

    A short example to explain how to use Classif.r

--- vmstools/R/ExApplicationForClassif.r
+++ vmstools/R/ExApplicationForClassif.r
@@ -20,14 +20,16 @@ dim(dateflalo)
 # FIRSTLY, YOU HAVE TO READ THE BEGINNING OF THE SOURCE FILE "CLASSIF.R" TO KNOW HOW TO USE THE FUNCTION "CLASSIF"
 # For example, if you have chosen HAC for the species selection, PCA and CLARA for the clustering of logevents, you have to write :
 dat=dateflalo
+analysisName=""
 methSpecies="hac"
 param1="euclidean"
 param2="ward"
 pcaYesNo="pca"
+criterion="70percents"
 methMetier="clara"
 param3="euclidean"
 param4=NULL
-cahacpclara=classif(dat,methSpecies,param1,param2,pcaYesNo,methMetier,param3,param4)
+cahacpclara=classif(dat,analysisName,methSpecies,param1,param2,pcaYesNo,criterion,methMetier,param3,param4)
 
 
 # Example
@@ -35,5 +37,5 @@ OTB2008euroNS=read.table(file="EFLALO_2008_OTB_EURO_NS.txt", sep=";",header = TR
 OTB2008euroNS=OTB2008euroNS[,3:ncol(OTB2008euroNS)]
 OTB2008euroNS[is.na(OTB2008euroNS)]=0
 dat=OTB2008euroNS 
-classif_OTB2008euroNS=classif(dat,"hac","euclidean","ward","pca","clara","euclidean",NULL)
+classif_OTB2008euroNS=classif(dat,"OTB2008euroNS","hac","euclidean","ward","pca","70percents","clara","euclidean",NULL)
 summary(classif_OTB2008euroNS)
