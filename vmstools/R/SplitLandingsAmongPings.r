97339969d41da8a001de59ce3044bf349481fb2c
doug.beare <doug.beare@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-11-04 07:54:11 +0000

    improving function to work on trips

--- vmstools/R/SplitLandingsAmongPings.r
+++ vmstools/R/SplitLandingsAmongPings.r
@@ -3,9 +3,17 @@ SplitLandingsAmongPings <- function(tacsat=tacsat, eflalo2=eflalo2, which.specie
 {
 #This function uses the output from count pings to distribute the catches of ONE chosen species recorded in eflalo over the pings in tacsat.
 #It then adds on the total weight and total landing values of all species in eflalo2
+
+#First sum logbook landings over the trips
+
+
+
 dn <- dimnames(eflalo2)[[2]]
 yp <- NULL
 for(ss in which.species){yp <- c(yp,grep(ss,dn)) }
+
+data <- aggregate(list(kgs=eflalo2[,yp[1]],cash=eflalo2[,yp[1]]), list(FT_REF=eflalo2$FT_REF), sum,na.rm=T)
+
 kgs <- eflalo2[,yp[1]][match(tacsat$FT_REF,eflalo2$FT_REF)]
 tacsat$kgs.by.ping <- kgs/tacsat$npings
 cash <- eflalo2[,yp[2]][match(tacsat$FT_REF,eflalo2$FT_REF)]
