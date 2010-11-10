f7de42b2a2831048a867c0ef79a8fbc831eeb583
fabrizio.manco <fabrizio.manco@cefas.co.uk>
2010-11-10 11:18:50 +0000

    New function to merge several ascii grids (outputs from vmsGridCreate.r) into a single grid

---
+++ vmstools/R/mosaicASCIIGrids.r
@@ -0,0 +1,25 @@
+## mosaicASCIIGrids.r
+## by Fabrizio Manco, 10/11/2010
+## merge a list of ASCII grids
+
+mosaicASCIIGrids <- function (ascgridfilelist,
+                             outputFileName)
+
+{
+  require(raster)
+  require(maptools)
+  grid1<-readAsciiGrid(ascgridfilelist[1])
+  mergedRaster<-raster(grid1)
+  rm(grid1)
+  
+  for (x in 2:length(ascgridfilelist)){
+    grid2<-readAsciiGrid(ascgridfilelist[x])
+    raster2<-raster(grid2)
+    
+    # do the merging and export as a ascii file
+    mergedRaster<-mosaic(mergedRaster, raster2, fun=sum)
+    }
+  writeRaster(mergedRaster, outputFileName, format="ascii")
+}
+
+
