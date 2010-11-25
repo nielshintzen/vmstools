99207c59afdc9d8feca73055d937f6113fe47a7e
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-11-25 11:22:24 +0000

    some few improvements i.e. x and y-axis and labels, par(mar), mapscale, and put points behind the legend

--- trunk/vmstools/R/mapGrid.r
+++ trunk/vmstools/R/mapGrid.r
@@ -19,6 +19,7 @@ mapGrid <- function( sGDF
                          , legendncol = 1
                          , legendtitle = "fishing activity"
                          , plotPoints = FALSE
+                         , addScale = TRUE
                          , outGridFile = ""  #name for output gridAscii
                          , outPlot = ""  #name for output png
                          , ... )
@@ -27,7 +28,8 @@ mapGrid <- function( sGDF
 require(sp)
 require(maptools)
 
-
+par(mar=c(4,6,1,1))
+ 
 xlim0=c(we,ea)
 ylim0=c(so,no)
 
@@ -46,12 +48,29 @@ library(mapdata)
 map("worldHires", add=TRUE,col="darkgreen",fill=TRUE,bg="white")
 box() # to put a box around the plot
 #mtext(paste(gear,year),font=4,line=-1.5)
+axis(1)
+axis(2, las=2)
+ if(we>0){
+   mtext("Degree East", side=1, line=2)
+   } else{
+   mtext("Degree West", side=1, line=2)
+   }
+ if(no>0){
+   mtext("Degree North", side=2, line=3)
+ } else{
+   mtext("Degree South", side=2, line=3)
+   }
+ 
+
+# add a scale
+if(addScale) map.scale(x=xlim0[2]-(xlim0[2]-xlim0[1])/2, y=ylim0[1], ratio=FALSE)
+
+#to add points (can obscure grid)
+if (plotPoints) points(sPDF, pch=".")
 
 #legend(x='bottomleft', legend=breaks[1:(length(breaks)-1)], pch = 22, pt.bg=cols, title="fishing activity",bg="white",pt.cex=2 )
 legend(x=legendx, legend=breaks0[1:(length(breaks0)-1)], pch = 22, pt.bg=cols, title=legendtitle, ncol=legendncol, bg="white",pt.cex=2 )
 
-#to add points (can obscure grid)
-if (plotPoints) points(sPDF, pch=".")
 
 #to add plotTitle
 if (plotTitle != "") mtext(plotTitle)
