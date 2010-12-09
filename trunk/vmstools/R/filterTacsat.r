908af61e98da9c71c5e04978ff3e893117028efd
niels.hintzen@gmail.com <niels.hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-12-09 08:31:05 +0000

    renaming of colnames removed

--- trunk/vmstools/R/filterTacsat.r
+++ trunk/vmstools/R/filterTacsat.r
@@ -5,11 +5,10 @@ function(tacsat
                          ,remDup=T                 #Specify if you want to remove duplicated VMS records (these should not occur in the first place)
                          ){
   VMS <- tacsat
-  colnames(VMS) <- c("ship","declat","declon","date","time","speed","heading")
   VMS$datim     <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M:%S")                                                   
   #Remove duplicate records
 if(is.null(remDup)==F){
-  uniqueVMS <- which(duplicated(VMS[,c("ship","datim")])==F)
+  uniqueVMS <- which(duplicated(VMS[,c("VE_REF","datim")])==F)
   VMS.      <- VMS[uniqueVMS,]
   if(dim(VMS.)[1] != dim(VMS)[1]) warning("duplicate records have been removed")
 } else {
@@ -19,9 +18,9 @@ if(is.null(remDup)==F){
 if(is.null(hd)==F){
   warning("It is assumed that VMS data is sorted by vessel and date")
     #Calculate the difference in heading between succeeding VMS datapoints
-  diffHeading                           <- abs(VMS.$heading[2:dim(VMS)[1]] - VMS.$heading[1:(dim(VMS)[1]-1)])
+  diffHeading                           <- abs(VMS.$SI_HE[2:dim(VMS)[1]] - VMS.$SI_HE[1:(dim(VMS)[1]-1)])
     #If there is a change to a new vessel in the data, put diffHeading to NA
-  diffHeading[unlist(lapply(as.list(unique(VMS.$ship)),function(x){return(which(VMS.$ship == x)[1])}))[-1]-1] <- NA
+  diffHeading[unlist(lapply(as.list(unique(VMS.$VE_REF)),function(x){return(which(VMS.$VE_REF == x)[1])}))[-1]-1] <- NA
   VMS.$diffHeading                      <- 0
     #Store the difference in heading
   VMS.$diffHeading[1:(dim(VMS.)[1]-1)]  <- diffHeading
@@ -32,7 +31,7 @@ if(is.null(hd)==F){
 if(is.null(st)==F){
     #Subset based on the speed ranges
   if(length(st)>2) stop("More than two speed values selected")
-  VMS. <- VMS.[which(VMS.$speed >= st[1] & VMS.$speed <= st[2]),]
+  VMS. <- VMS.[which(VMS.$SI_SP >= st[1] & VMS.$SI_SP <= st[2]),]
 }
 colnames(VMS.) <- c(colnames(tacsat),"SI_DATIM")
 return(VMS.[,-grep("SI_DATIM",colnames(VMS.))])}
