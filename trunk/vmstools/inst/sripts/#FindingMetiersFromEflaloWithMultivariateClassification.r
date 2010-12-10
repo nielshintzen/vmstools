c5e69f4b7fcd6a949cd32f5d7d6fc1433697306f
nicolas.deporte@gmail.com <nicolas.deporte@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-12-10 13:47:14 +0000

    Addition : - combine data at the beginning of the code - step 4 and 5 at the end

--- trunk/vmstools/inst/sripts/#FindingMetiersFromEflaloWithMultivariateClassification.r
+++ trunk/vmstools/inst/sripts/#FindingMetiersFromEflaloWithMultivariateClassification.r
@@ -25,15 +25,15 @@ setwd(path) # you must choose the path of your working directory
 source("programs/FunctionsForClassif.r")
 source("programs/Classif.r")
 source("programs/ExploSpeciesSelection.r")
-memory.limit(size=2000)
+memory.limit(size=4000)
 
 
 
 #-----------------------------
 # I. GETTING THE DATA IN AND CLEANING FOR MISSING AND NEGATIVE VALUES ETC
 #-----------------------------
-# 
-country <- "DK"
+ 
+country <- "SCO"
 
 year <- 2007
 AreaCodename <- "3a4"
@@ -42,10 +42,32 @@ Gear <- c("OTB")
 analysisName=paste(country,"_",Gear,year,"_",AreaCodename,sep="")
 
 # load your own dataset (called dat1 here)
-load(paste("data/DK_eflalo_",year,Gear,AreaCodename,".Rdata",sep=""))
+load(paste("data/",country,"_eflalo_",year,Gear,AreaCodename,".Rdata",sep=""))
 
- # creating the directory of the analysis
- if (!file.exists(analysisName)) dir.create(analysisName)
+
+# combined data
+dat <- read.table("data/FRA-DK-NLD_EFLALO_2007_OTB_EURO_NS-2.txt",dec=",",sep=";",header=TRUE)
+dat$LE_ID <- paste(dat$COUNTRY,dat$LE_ID,sep=".")
+dat <- dat[,c("LE_ID",grep("EURO",names(dat),value=T))]
+
+
+sco <- read.csv("data/OTB_2007_SCO.csv")
+sco$LE_ID <- paste("SCO",sco$LE_ID,sep=".")
+sco <- sco[,c("LE_ID",grep("EURO",names(sco),value=T))]
+
+combined_columns <- unique(c(names(dat),names(sco)))
+
+dat[,combined_columns[is.na(match(combined_columns,names(dat)))]] <- NA
+sco[,combined_columns[is.na(match(combined_columns,names(sco)))]] <- NA
+
+dat1 <- rbind(dat,sco)
+
+rm(dat,sco)
+gc()
+
+
+# creating the directory of the analysis
+if (!file.exists(analysisName)) dir.create(analysisName)
 setwd(paste(path,analysisName,sep=""))
 #delete old R cache
 if (file.exists(".R_Cache")) unlink(".R_Cache",recursive=TRUE)      
@@ -54,7 +76,7 @@ if (file.exists(".R_Cache")) unlink(".R_Cache",recursive=TRUE)
 eflalo_ori <- dat1 # keeping this in cached memory for making the final merging at the end
 Store(eflalo_ori)
 
- #! KEEPING ONLY LE_ID AND THE OUTPUT YOU WANT TO GET  (KG/EURO)
+#! KEEPING ONLY LE_ID AND THE OUTPUT YOU WANT TO GET  (KG/EURO)
 dat1 <- dat1[,c("LE_ID",grep("EURO",names(dat1),value=T))]
 dat1[is.na(dat1)]=0
 
@@ -76,13 +98,16 @@ dat1 <- dat1[,!names(dat1)=="MZZ"]
 #-----------------------------
 # II. EXPLORING THE VARIOUS METHODS FOR IDENTIFYING MAIN SPECIES AND KEEPING THEM IN THE DATA SET (STEP 1)
 #-----------------------------
- #EXPLORATION
+
+#EXPLORATION
 explo=ExploSpeciesSelection(dat1,analysisName,RunHAC=TRUE,DiagFlag=FALSE)
 
 # Step 1 : selection of main species
 Step1=classif_step1(dat1,explo$NamesMainSpeciesHAC,paramTotal=95,paramLogevent=100)
 
 save(explo,Step1,file="Explo_Step1.Rdata")
+
+
 #-----------------------------
 # III. STEP 2 - PCA - NO-PCA
 #-----------------------------
@@ -102,9 +127,12 @@ if (option_step2=="PCA_SC") Step2=classif_step2(Step1,analysisName,pcaYesNo="pca
 if (option_step2=="NO_PCA") Step2=classif_step2(Step1,analysisName,pcaYesNo="nopca",criterion=NULL)    
 
 save(Step2,file="Step2.Rdata")
+
+
 #-----------------------------
 # IV. STEP 3 - CLUSTERING METHOD : HAC, CLARA OR KMEANS
 #-----------------------------
+
 for (option_step3 in c("CLARA","KMEANS")) {
 
 setwd(paste(path,analysisName,"/",option_step2,sep=""))
@@ -120,6 +148,10 @@ if (option_step3=="KMEANS") Step3=classif_step3(Step1,Step2,analysisName=analysi
 save(Step3,file="Step3.Rdata")
 } # end of step 3
 } # end of step 2
+
+
+
+
 #-----------------------------
 # V. STEP 4 - MERGING BACK TO EFLALO
 #-----------------------------
@@ -129,10 +161,58 @@ setwd(paste(path,analysisName,sep=""))
 
 load("PCA_70/CLARA/Step3.Rdata")
 
- if(!nrow(dat1)==nrow(Step3$LE_ID_clust)) print("--error : number of lines in step 3 not equal to input eflalo, please check!!--")
+if(!nrow(dat1)==nrow(Step3$LE_ID_clust)) print("--error : number of lines in step 3 not equal to input eflalo, please check!!--")
 
 dat1 <- cbind(dat1,CLUSTER=Step3$LE_ID_clust[,"clust"])
 #now reload the full data set 
 
 eflalo_ori[-sort(unique(null.value)),"CLUSTER"] <- Step3$LE_ID_clust[,"clust"] 
 
+
+
+
+#-----------------------------
+# VI. STEP 5 - Predicting Metier for current year using clustering performed previous year
+#-----------------------------
+ 
+new_country <- "SCO"
+new_year <- 2008
+new_AreaCodename <- "3a4"
+new_Gear <- c("OTB")
+# load previous R objects (Step1,Step2,Step3)
+setwd(path)
+load(paste(analysisName,"/PCA_70/CLARA/Step3.Rdata",sep=""))
+load(paste(analysisName,"/PCA_70/Step2.Rdata",sep=""))
+load(paste(analysisName,"/Explo_Step1.Rdata",sep=""))
+
+# load your new dataset (called dat1 here)
+#! KEEPING ONLY LE_ID AND THE OUTPUT YOU WANT TO GET  (KG/EURO)
+load(paste("data/",new_country,"_eflalo_",new_year,new_Gear,new_AreaCodename,".Rdata",sep=""))
+datPred <- dat1[,c("LE_ID",grep("EURO",names(dat1),value=T))]
+datPred[is.na(datPred)]=0
+
+#removing negative and null values
+null.value <- vector()
+for (i in grep("EURO",names(datPred))) null.value <- c(null.value,which(datPred[,i]<0))
+null.value <- c(null.value,which(apply(datPred[,2:ncol(datPred)],1,sum,na.rm=T)==0))
+
+if(length(null.value)!=0) {LogEvent.removed <- datPred[sort(unique(null.value)),] ; datPred <- datPred[-sort(unique(null.value)),]}
+Store(LogEvent.removed)
+
+names(datPred)[-1]=unlist(lapply(strsplit(names(datPred[,-1]),"_"),function(x) x[[3]]))
+
+#removing miscellaneous species
+
+datPred <- datPred[,!names(datPred)=="MZZ"]
+
+#create a new folder for step5
+option_step2="PCA_70"
+option_step3="CLARA"
+option_step5 = "Predict_2008"
+setwd(paste(path,analysisName,"/",option_step2,"/",option_step3,sep=""))
+if (!file.exists(option_step5)) dir.create(option_step5)
+setwd(paste(path,analysisName,"/",option_step2,"/",option_step3,"/",option_step5,sep=""))
+if (file.exists(".R_Cache")) unlink(".R_Cache",recursive=TRUE)
+
+
+Donnees2008Cluster = metierPredict(learningData=Step1,clustersAffectation=Step3$clusters$clustering,newData=datPred)
