e818a575aa08017b77568e0d7bbb391c3e4e4b72
nicolas.deporte@gmail.com <nicolas.deporte@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-12-16 17:13:10 +0000

    Determination of metiers level 5 from : - classif_step3 - first species in catch - first group of species (level 5) in catch Comparaison of the results obtained via the 3 methods

--- trunk/vmstools/inst/sripts/#FromLevel7toLevel5.r
+++ trunk/vmstools/inst/sripts/#FromLevel7toLevel5.r
@@ -33,45 +33,49 @@ spp <-lapply(colnames(mean_),function(x) which(mean_[,x]>=10))  # this gives the
 
 # linking to level 5
 
-level5 <- lapply(spp, function(x) {
-  assemblage <- NULL
-  if(length(x)==0){   assemblage <- c(assemblage,"NA") 
-  }else{
-    for (i in seq(length(x))) {
-      if (names(x[i]) %in% c('NEP','PRA','CRK','CRB','CRG','CRJ','CRA','CRQ','CRR','KCT','LBA','LBE','AES','PEN','PAN','CRU','ERS','CSH','CRE','KEF','SLO','CPR','LIO','SCR'))  assemblage <- c(assemblage,"CRU") else
-      if (names(x[i]) %in% c('BLL','COD','HAD','RED','REG','REB','REN','HKS','HKR','PLA','WIT','YEL','GHL','HAL','FLW','FLS','FLD','FLX','FLY','ANG','SRA','TOM','ANT','CUN','USK','GRC','BLI','LIN','LUM',
-              'KGF','PUF','ELZ','OPT','POC','RNG','RHG','SCU','CAT','CAA','CAS','GRO','BSS','GUG','LEM','LEZ','SRA','DAB','FLE','PLE','POK','POL','SAN','SOL','WHG','TUR','APO','ANF','ARU',
-              'COE','DGS','DGX','LEF','LEM','MUX','MEG','SBR','SKA','SYC','SYT','SDV','GAG','AGN','JRS','RAJ','JOD','MGS','WEG','CTZ','GUR','SOS','MNZ','BIB','GUU','MGR','MGS','MUR','RJC',
-              'RJM','MON'))  assemblage <- c(assemblage,"DEF") else # "Demersal_fish"  # FAO sp code
-      if (names(x[i]) %in% c('ABR','HLT','KRS','MUS','KNK','KMS','SNE','MOL','CLR','CLH','CLQ','CLS','CLT','COC','SCB','SCC','ISC','SCA','SCX','OYA','WHX','PER','WHE','QSC','VSC','MYV','VEV','GKL','SCE'))  assemblage <- c(assemblage,"MOL") else # "Molluscs"  # FAO sp code
-      if (names(x[i]) %in% c('ELE','STU'))  assemblage <- c(assemblage,"CAT") else # "Catadromus_species"   # FAO sp code
-      if (names(x[i]) %in% c('ANE','HER','SPR','MAC','HKE','SAU','ANB','SSM','MHA','BUT','CAP','PIL','JAX','SHG','ARU','ARY','MAS','GAR','MUL','WHB','NOP','HOM'))  assemblage <- c(assemblage,"SPF") else # "Small_pelagic_fish"  # FAO sp code
-      if (names(x[i]) %in% c('BLU','CVJ','FRI','KGM','SSM','SAI','WHM','BUM','SWO','ALB','BON','LTA','BET','BFT','SKJ','YFT','POR','DGS','GUQ','DGX','SRX','SKA','SRX','TUN','GUQ','SBG'))  assemblage <- c(assemblage,"LPF") else # "Large_pelagic_fish"  # FAO sp code
-      if (names(x[i]) %in% c('CHR','SAL','APU','TRS','TRR','SLZ','TLV','PLN','VIV','LAR','STU','SME','TRO','SHG','SHD','PLN','LAU'))  assemblage <- c(assemblage,"ANA") else # "Anadromous_species"
-      if (names(x[i]) %in% c('FBM','FPE','FPI','PLN','FBR','FCP','FTE','BRB','FBR','FBU','FCC','FCP','FPP','FRO','SAR','ELP','FFX'))  assemblage <- c(assemblage,"FWS") else # "Freshwater_species"
-      if (names(x[i]) %in% c('SQL','SQI','SQU','OCC','CTC','SQR','IAX','SQZ','OMZ'))  assemblage <- c(assemblage,"CEP") else # "Cephalopods"
-      if (names(x[i]) %in% c('HYD','ORY')) assemblage <- c(assemblage,"DWS") else  # "Deep_Water_Fish"
-      if (names(x[i]) %in% c('MZZ','OTH','FRF','YYY','ZZZ'))  assemblage <- c(assemblage,"OTH") # "Other"  # FAO sp code
-    }
-  }
-  assemblage <- do.call(paste,as.list(unique(assemblage)))
-})
+level5 <- lapply(spp, Level5)
 
 
+# Determine the metier of each logevent from dat thanks to the first species in catch of the logevent and compare with the results of classif_step3 method.
+first=firstSpecies(dat1,Step2,Step3$clusters$clustering)
+# Determine the metier in level 5 from the metier in level 7 based on the first species in catch (logevent by logevent)
+logSpeciesL5=sapply(first$firstSp,function(x) Level7to5(x))
+unique(logSpeciesL5)
+barplot(table(logSpeciesL5), main="Number of logevents by group of species level 5", xlab="Groups of species level 5", ylab="Number of logevents")
 
-# Determine the metier of each logevent from dat thanks to the first species in catch of the logevent and compare with the results of classif_step3.
-first=firstSpecies(dat1,classif2,classif3$clusters)
-# Determine the metier in level 5 from the metier in level 7 based on the first species catched logevent by logevent
-L5=sapply(first$firstSp,function(x) Level7to5(x))
-unique(L5)
-barplot(table(L5))
 
 # Determine the metier of each logevent thanks to the first group of species (level 5) in catch of the logevent
-colnames(dat)=sapply(colnames(dat), Level7to5)
+
+dat=as.matrix(dat1[,2:ncol(dat1)])
+colnames(dat)=sapply(as.list(colnames(dat)), Level7to5)
 nbC5=length(unique(colnames(dat)))
 tab=matrix(NA,nrow=nrow(dat),ncol=nbC5)
 colnames(tab)=unique(colnames(dat))
 for(i in 1:nbC5){
-  tab[,i]=apply(dat,1,function(x) sum(dat[x,which(colnames(dat)==unique(colnames(dat))[i])]))
+  subdati=dat[,which(colnames(dat)==(unique(colnames(dat))[i]))]
+  if(length(which(colnames(dat)==(unique(colnames(dat))[i])))==1) tab[,i]=subdati
+  else
+    tab[,i]=apply(subdati,1,sum)
 }
-clust5GP=unique(colnames(dat))[apply(tab,1,which.max)]
+# Metiers level 5 of each logevent found thanks to the first group of species in catch (level 5)
+logGroupL5=unique(colnames(dat))[apply(tab,1,which.max)]
+
+# Metiers level 5 of each logevent found thanks to the classif_step3 method.
+clustersStep3L5=unlist(sapply(Step3$clusters$clustering,function(x) level5[x]))
+
+
+# Compare the metiers (level 5) found thanks to the classif (Step3) vs the first group of species (level 5) in catch
+#compL5ClustStep3VsGroup=table(clustersStep3L5,logGroupL5)  !! pb regroupement des clusters qui correspondent au même groupe d'espèce niveau 5.
+clustStep3L5=Step3$clusters$clustering
+compL5ClustStep3VsGroup=table(clustStep3L5,logGroupL5)
+rownames(compL5ClustStep3VsGroup)=unlist(level5)
+
+# Compare the metiers (level 5) found thanks to the first species in catch vs the first group of species in catch
+compL5SpeciesVsGroup=table(logSpeciesL5,logGroupL5)
+
+# Compare the metiers (level 5) found thanks to the classif (Step3) vs the first species in catch
+#compL5ClustStep3VsSpecies=table(clustersStep3L5,logSpeciesL5)    !! pb regroupement des clusters qui correspondent au même groupe d'espèce niveau 5.
+clustStep3L5=Step3$clusters$clustering
+compL5ClustStep3VsSpecies=table(clustStep3L5,logSpeciesL5)
+rownames(compL5ClustStep3VsSpecies)=unlist(level5)
+
