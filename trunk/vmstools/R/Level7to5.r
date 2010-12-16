d13a67e84d2f1fdd6bc09b2caa0599917ef48359
nicolas.deporte@gmail.com <nicolas.deporte@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-12-16 17:09:24 +0000

    Correspondance level 7 to level 5 for matrix of characters (species) Adding some species.

--- trunk/vmstools/R/Level7to5.r
+++ trunk/vmstools/R/Level7to5.r
@@ -1,4 +1,4 @@
-# define assemblage of species according to DCR
+# Define assemblage of species according to DCR
 
 Level7to5=function(x){
   if(x %in% c('NEP','PRA','CRK','CRB','CRG','CRJ','CRA','CRQ','CRR','KCT','LBA','LBE','AES','PEN','PAN','CRU','ERS','CSH','CRE','KEF','SLO','CPR','LIO','SCR')) x <- "CRU" # "Crustaceans" # FAO sp code
@@ -6,9 +6,9 @@ Level7to5=function(x){
   if(x %in% c('BLL','COD','HAD','RED','REG','REB','REN','HKS','HKR','PLA','WIT','YEL','GHL','HAL','FLW','FLS','FLD','FLX','FLY','ANG','SRA','TOM','ANT','CUN','USK','GRC','BLI','LIN','LUM',
               'KGF','PUF','ELZ','OPT','POC','RNG','RHG','SCU','CAT','CAA','CAS','GRO','BSS','GUG','LEM','LEZ','SRA','DAB','FLE','PLE','POK','POL','SAN','SOL','WHG','TUR','APO','ANF','ARU',
               'COE','DGS','DGX','LEF','LEM','MUX','MEG','SBR','SKA','SYC','SYT','SDV','GAG','AGN','JRS','RAJ','JOD','MGS','WEG','CTZ','GUR','SOS','MNZ','BIB','GUU','MGR','MGS','MUR','RJC',
-              'RJM','MON')) x <- "DEF" # "Demersal_fish"  # FAO sp code
+              'RJM','MON','RJN','RJE')) x <- "DEF" # "Demersal_fish"  # FAO sp code
 
-  if(x %in% c('ABR','HLT','KRS','MUS','KNK','KMS','SNE','MOL','CLR','CLH','CLQ','CLS','CLT','COC','SCB','SCC','ISC','SCA','SCX','OYA','WHX','PER','WHE','QSC','VSC','MYV','VEV','GKL','SCE')) x <- "MOL" # "Molluscs"  # FAO sp code
+  if(x %in% c('ABR','HLT','KRS','MUS','KNK','KMS','SNE','MOL','CLR','CLH','CLQ','CLS','CLT','COC','SCB','SCC','ISC','SCA','SCX','OYA','WHX','PER','WHE','QSC','VSC','MYV','VEV','GKL','SCE','OYF','OYG')) x <- "MOL" # "Molluscs"  # FAO sp code
 
   if(x %in% c('ELE','STU')) x <- "CAT" # "Catadromus_species"   # FAO sp code
 
@@ -20,7 +20,7 @@ Level7to5=function(x){
 
   if (x %in% c('FBM','FPE','FPI','PLN','FBR','FCP','FTE','BRB','FBR','FBU','FCC','FCP','FPP','FRO','SAR','ELP','FFX')) x <- "FWS" # "Freshwater_species"
 
-  if(x %in% c('SQL','SQI','SQU','OCC','CTC','SQR','IAX','SQZ','OMZ')) x <- "CEP" # "Cephalopods"
+  if(x %in% c('SQL','SQI','SQU','OCC','CTC','SQR','IAX','SQZ','OMZ','CTL')) x <- "CEP" # "Cephalopods"
   
   if(x %in% c('HYD','ORY')) x <- "DWS" # "Deep_Water_Fish"
 
