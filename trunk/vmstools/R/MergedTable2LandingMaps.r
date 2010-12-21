53c9cd5b1a10c285fb52ed6306e1b50a34cd7e3c
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2010-12-21 09:43:38 +0000

    small adjustments

--- trunk/vmstools/R/MergedTable2LandingMaps.r
+++ trunk/vmstools/R/MergedTable2LandingMaps.r
@@ -7,44 +7,43 @@
     if(!"quarter" %in% colnames(all.merged))
           stop("please add a 'quarter' column to the merged table")
 
+    # create the general folder
     dir.create(file.path(output, "jpegLandings"))
 
+    # detect the year
     a.year <- format(strptime( paste(all.merged$SI_DATE[1]) , tz='GMT',  "%e/%m/%Y" ), "%Y")
 
     if (any(grep("EURO", sp)>0)) what <- "value"
     if (any(grep("KG", sp)>0))  what <- "weight"
-
     if(what=="weight") a.unit <- "(KG)"
     if(what=="value") a.unit <- "(EURO)"
 
     get.sp <- function (nm) unlist(lapply(strsplit(nm, split="_"), function(x) x[3]))
     a.sp <- get.sp(sp)
 
-    df1 <- all.merged[all.merged$SI_STATE==1, colnames(all.merged)%in% c("SI_LATI","SI_LONG",sp)]
-    df1$SI_LATI <- as.numeric(as.character(df1$SI_LATI )) # debug...
-    df1$SI_LONG <- as.numeric(as.character(df1$SI_LONG )) # debug...
+    df1 <- all.merged[all.merged$SI_STATE==1, c("SI_LATI","SI_LONG",sp)]
+    df1$SI_LATI <- anf(df1$SI_LATI ) # debug...
+    df1$SI_LONG <- anf(df1$SI_LONG ) # debug...
     df1[,sp] <- replace(df1[,sp], is.na(df1[,sp]) | df1[,sp]<0, 0)
-    vmsGridCreate(df1, nameVarToSum=sp,  numCats=10,  plotPoints =FALSE, legendtitle=paste("landing",what,a.unit,sep=' '),
+    vmsGridCreate(df1, nameVarToSum=sp,  numCats=10,  plotPoints =FALSE, legendtitle=paste("landings",what,a.unit,sep=' '),
           colLand="darkolivegreen4",  addICESgrid=TRUE,
-            nameLon="SI_LONG", nameLat="SI_LATI",cellsizeX =cellsizeX, cellsizeY =cellsizeY, we=we, ea=ea, no=no, so=so,
+            nameLon="SI_LONG", nameLat="SI_LATI", cellsizeX =cellsizeX, cellsizeY =cellsizeY, we=we, ea=ea, no=no, so=so,
              breaks0=breaks0, legendncol=2)
     title(a.sp)
-          # create folders
+          # create folders and save
     dir.create(file.path(output, "jpegLandings", a.sp))
     dir.create(file.path(output, "jpegLandings", a.sp, "overall"))
     savePlot(filename = file.path(output, "jpegLandings", a.sp, "overall",
-                            paste("map_landings_kg_merged_vessels_",what,"_",a.sp,"_",a.year,".jpeg",sep="")),type ="jpeg")
+                            paste("map_landings_",what,"_merged_vessels_",what,"_",a.sp,"_",a.year,".jpeg",sep="")),type ="jpeg")
 
     dev.off()
 
-    # per fleet
+    # per metier
     for (met in levels(all.merged$LE_MET_level6) ){
-
-
       df1 <- all.merged[all.merged$LE_MET_level6==met &
-                all.merged$SI_STATE==1, colnames(all.merged)%in% c("SI_LATI","SI_LONG",sp)]
-      df1$SI_LATI <- as.numeric(as.character(df1$SI_LATI )) # debug...
-      df1$SI_LONG <- as.numeric(as.character(df1$SI_LONG )) # debug...
+                all.merged$SI_STATE==1, c("SI_LATI","SI_LONG",sp)]
+      df1$SI_LATI <- anf(df1$SI_LATI) # debug...
+      df1$SI_LONG <- anf(df1$SI_LONG) # debug...
       df1[,sp] <- replace(df1[,sp], is.na(df1[,sp]) | df1[,sp]<0, 0)
       if(nrow(df1)!=0){
       vmsGridCreate(df1, nameVarToSum=sp,  numCats=10,  plotPoints =FALSE, legendtitle=paste("landings",what,a.unit,sep=' '),
@@ -52,38 +51,39 @@
             nameLon="SI_LONG", nameLat="SI_LATI",cellsizeX =cellsizeX, cellsizeY =cellsizeY, we=we, ea=ea, no=no, so=so,
              breaks0=breaks0,legendncol=2)
          title(paste(met, "-", a.sp) )
-      a.met <- gsub(">=", replacement="o",met)
-      a.met <- gsub("<", replacement="",a.met)
-      a.met <- gsub(">", replacement="o",a.met)
+      a.met <- gsub(">=", replacement="o",met) # debug
+      a.met <- gsub("<", replacement="",a.met)  # debug
+      a.met <- gsub(">", replacement="o",a.met)  # debug
       # create folders and save
       dir.create(file.path(output, "jpegLandings", a.sp, a.met))
       savePlot(filename = file.path(output, "jpegLandings", a.sp, a.met,
-                            paste("map_landings_kg_merged_vessels_",what,"_",a.sp,"_", a.met,"_",a.year,".jpeg",sep="")),type ="jpeg")
+                            paste("map_landings_",what,"_merged_vessels_",what,"_",a.sp,"_", a.met,"_",a.year,".jpeg",sep="")),type ="jpeg")
       dev.off()
       }
    }
 
-   # per fleet, quarter
+   # per metier, quarter
    for (met in levels(all.merged$LE_MET_level6) ){
      for (a.quarter in levels(all.merged$quarter) ){
 
       df1 <- all.merged[all.merged$LE_MET_level6==met &
                 all.merged$quarter==a.quarter  &
-                all.merged$SI_STATE==1, colnames(all.merged)%in% c("SI_LATI","SI_LONG",sp)]
-      df1$SI_LATI <- as.numeric(as.character(df1$SI_LATI )) # debug...
-      df1$SI_LONG <- as.numeric(as.character(df1$SI_LONG )) # debug...
+                all.merged$SI_STATE==1,  c("SI_LATI","SI_LONG",sp)]
+      df1$SI_LATI <- anf(df1$SI_LATI) # debug...
+      df1$SI_LONG <- anf(df1$SI_LONG) # debug...
       df1[,sp] <- replace(df1[,sp], is.na(df1[,sp]) | df1[,sp]<0, 0)
       if(nrow(df1)!=0){
-      vmsGridCreate(df1, nameVarToSum=sp,  numCats=10,  plotPoints =FALSE, legendtitle=paste("landing",what,a.unit,sep=' '),
+      vmsGridCreate(df1, nameVarToSum=sp,  numCats=10,  plotPoints =FALSE, legendtitle=paste("landings",what,a.unit,sep=' '),
           colLand="darkolivegreen4",  addICESgrid=TRUE,
-            nameLon="SI_LONG", nameLat="SI_LATI",cellsizeX =cellsizeX, cellsizeY =cellsizeY, we=we, ea=ea, no=no, so=so,
+            nameLon="SI_LONG", nameLat="SI_LATI", cellsizeX =cellsizeX, cellsizeY =cellsizeY, we=we, ea=ea, no=no, so=so,
              breaks0=breaks0,legendncol=2)
          title(paste(met, "-", a.sp) )
       a.met <- gsub(">=", replacement="o",met) # debug
       a.met <- gsub("<", replacement="",a.met) # debug
       a.met <- gsub(">", replacement="o",a.met)# debug
+      # save
       savePlot(filename = file.path(output, "jpegLandings", a.sp, a.met,
-        paste("map_landings_kg_merged_vessels_",what,"_",a.sp,"_", a.met,"_",a.year,"_",a.quarter,".jpeg",sep="")),type ="jpeg")
+        paste("map_landings_",what,"_merged_vessels_",what,"_",a.sp,"_", a.met,"_",a.year,"_",a.quarter,".jpeg",sep="")),type ="jpeg")
       dev.off()
       }
      }}
