c14a61cad6fb89ee4784666ab3bea620d505706c
niels.hintzen@gmail.com <niels.hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-01-05 10:27:43 +0000

    readEflalo & readTacsat added as function including help file

---
+++ trunk/vmstools/R/readEflalo.r
@@ -0,0 +1,20 @@
+readEflalo <- function(file,sep=",",dec="."){
+
+                    #Read the data
+                  res <- read.table(file, header = T,sep,dec = ".",stringsAsFactors = F)
+
+                    #Perform checks
+                  if(any(!c("VE_REF","VE_FLT","VE_COU","VE_LEN","VE_KW","VE_TON","FT_REF","FT_DCOU","FT_DHAR","FT_DDAT","FT_DTIME",
+                            "FT_LCOU","FT_LHAR","FT_LDAT","FT_LTIME","LE_ID","LE_CDAT","LE_STIME","LE_ETIME","LE_SLAT","LE_SLON",
+                            "LE_ELON","LE_ELON","LE_GEAR","LE_MSZ") %in% colnames(res)))
+                              stop(paste(file,"needs correct header including",paste(c("VE_REF","VE_FLT","VE_COU","VE_LEN","VE_KW","VE_TON","FT_REF","FT_DCOU","FT_DHAR","FT_DDAT","FT_DTIME",
+                                  "FT_LCOU","FT_LHAR","FT_LDAT","FT_LTIME","LE_ID","LE_CDAT","LE_STIME","LE_ETIME","LE_SLAT","LE_SLON",
+                                  "LE_ELON","LE_ELON","LE_GEAR","LE_MSZ"))))
+                  if(length(grep("KG",colnames(eflalo))) < 1) stop("Units of landings are not in KG")
+                  if(length(grep("LE_KG",colnames(eflalo))) < 1) stop("No landing data provided")
+                  if(length(grep("LE_EURO",colnames(eflalo))) < 1) warning("Currency used is different than 'EURO'")
+
+                    #Reformat the data
+                  res <- formatEflalo(res)
+
+              return(res)}
---
+++ trunk/vmstools/R/readTacsat.r
@@ -0,0 +1,14 @@
+readTacsat <- function(file,sep=",",dec="."){
+
+                    #Read the data
+                  res <- read.table(file, header = T,sep,dec = ".",stringsAsFactors = F)
+
+                    #Perform checks
+                  if(any(!c("VE_REF","SI_LATI","SI_LONG","SI_DATE","SI_TIME","SI_SP","SI_HE") %in% colnames(res))) stop(paste(file,"needs correct header including",paste("VE_REF","SI_LATI","SI_LONG","SI_DATE","SI_TIME","SI_SP","SI_HE")))
+                  if(any(res$SI_LATI > 90 | res$SI_LATI < -90,na.rm=T) | any(res$SI_LONG > 180 | res$SI_LONG < -180,na.rm=T)) stop("Longitudes or latitudes are out of range")
+                  if(any(res$SI_HE > 360 | res$SI_HE < 0,na.rm=T)) stop("Heading out of range, must be between 0 - 360")
+
+                    #Reformat the data
+                  res <- formatTacsat(res)
+
+              return(res)}
