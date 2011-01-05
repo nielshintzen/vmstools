5f51b4648e414afccf3525607ef4dfc58befe349
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-01-05 15:04:03 +0000

    renaming function mergedTable2LandingsMaps.r into pings2LandingsMaps.r

--- vmstools/R/pings2LandingsMaps.r
+++ vmstools/R/pings2LandingsMaps.r
@@ -1,5 +1,5 @@
   # Author: F.Bastardie
-  mergedTable2LandingsMaps <-
+  pings2LandingsMaps <-
      function (all.merged, sp="LE_KG_COD", output= file.path("C:","VMSanalysis", "FemernBelt"),
         cellsizeX =0.05, cellsizeY =0.05, we=9.8, ea=12.7, no=55.2, so=54.0,
           breaks0= c(0,100, 100*(2^1),100*(2^2),100*(2^3),100*(2^4),100*(2^5),100*(2^6), 100*(2^7),100*(2^8),100*(2^9), 10000000)){
@@ -117,7 +117,7 @@
  return()
  }
   #  in value
- # mergedTable2LandingsMaps (all.merged=all.merged, sp="LE_EURO_COD", output= file.path("C:","VMSanalysis", "FemernBelt"),
+ # pings2LandingsMaps (all.merged=all.merged, sp="LE_EURO_COD", output= file.path("C:","VMSanalysis", "FemernBelt"),
  #                   cellsizeX =0.05, cellsizeY =0.05, we=9.8, ea=12.7, no=55.2, so=54.0, # fehmarn Belt area
  #                       breaks0= c(0,100, 100*(2^1),100*(2^2),100*(2^3),100*(2^4),100*(2^5),100*(2^6), 100*(2^7),100*(2^8),100*(2^9), 10000000)
  #                       )
