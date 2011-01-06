15367f54e70ecf37a4b00032cc7773e27f2043b8
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-01-06 10:01:32 +0000

    correct the name of the function

--- trunk/vmstools/R/pings2Fishframe.r
+++ trunk/vmstools/R/pings2Fishframe.r
@@ -1,6 +1,6 @@
 
 
- mergedTable2Fishframe <- function(general=list(output.path=
+ pings2Fishframe <- function(general=list(output.path=
              file.path("H:","DIFRES","VMSanalysis","results_merged","DKWaters"),
                                           a.year=2009, a.country="DNK")){
                                           
@@ -134,7 +134,7 @@
          # (tricky because sub-block by sub-block because of potential 'out of memory')
          res <- NULL
          lev <- as.character(levels(xx1$c_square)) # do not split randomly but consistently with levels
-         chunk  <- c( seq(1, length(lev), by=2000), length(lev)) # 4000 by 4000 levels...
+         chunk  <- c( seq(1, length(lev), by=2000), length(lev)) # 2000 by 2000 levels...
          for(i in 1: (length(chunk)-1)){
             rm(vsl.ff1,vsl.ff2,vsl.ff) ; gc(reset=TRUE)
             cat(paste("level c_square",chunk[i],"to",chunk[i+1] ,"\n"))
