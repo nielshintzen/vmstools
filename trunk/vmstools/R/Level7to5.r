12ea427a01d62300ac6a8cd34fe9696ddcf6e815
nicolas.deporte@gmail.com <nicolas.deporte@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-01-10 08:51:57 +0000

    Version : 10/01/11

--- trunk/vmstools/R/Level7to5.r
+++ trunk/vmstools/R/Level7to5.r
@@ -1,28 +1,48 @@
 # Define assemblage of species according to DCR
 
 Level7to5=function(x){
-  if(x %in% c('NEP','PRA','CRK','CRB','CRG','CRJ','CRA','CRQ','CRR','KCT','LBA','LBE','AES','PEN','PAN','CRU','ERS','CSH','CRE','KEF','SLO','CPR','LIO','SCR')) x <- "CRU" # "Crustaceans" # FAO sp code
+  if(x %in% c('NEP','PRA','CRK','CRB','CRG','CRJ','CRA','CRQ','CRR','KCT','LBA',
+  'LBE','AES','PEN','PAN','CRU','ERS','CSH','CRE','KEF','SLO','CPR','LIO','SCR',
+  'PSL','LOQ','PIQ')) x <- "CRU" # "Crustaceans" # FAO sp code
   
-  if(x %in% c('BLL','COD','HAD','RED','REG','REB','REN','HKS','HKR','PLA','WIT','YEL','GHL','HAL','FLW','FLS','FLD','FLX','FLY','ANG','SRA','TOM','ANT','CUN','USK','GRC','BLI','LIN','LUM',
-              'KGF','PUF','ELZ','OPT','POC','RNG','RHG','SCU','CAT','CAA','CAS','GRO','BSS','GUG','LEM','LEZ','SRA','DAB','FLE','PLE','POK','POL','SAN','SOL','WHG','TUR','APO','ANF','ARU',
-              'COE','DGS','DGX','LEF','LEM','MUX','MEG','SBR','SKA','SYC','SYT','SDV','GAG','AGN','JRS','RAJ','JOD','MGS','WEG','CTZ','GUR','SOS','MNZ','BIB','GUU','MGR','MGS','MUR','RJC',
-              'RJM','MON','RJN','RJE')) x <- "DEF" # "Demersal_fish"  # FAO sp code
+  if(x %in% c('BLL','COD','HAD','RED','REG','REB','REN','HKS','HKR','PLA','WIT',
+  'YEL','GHL','HAL','FLW','FLS','FLD','FLX','FLY','ANG','SRA','TOM','ANT','CUN',
+  'USK','GRC','BLI','LIN','LUM','KGF','PUF','ELZ','OPT','POC','RNG','RHG','SCU',
+  'CAT','CAA','CAS','GRO','BSS','GUG','LEM','LEZ','SRA','DAB','FLE','PLE','POK',
+  'POL','SAN','SOL','WHG','TUR','APO','ANF','ARU','COE','DGS','DGX','LEF','LEM',
+  'MUX','MEG','SBR','SKA','SYC','SYT','SDV','GAG','AGN','JRS','RAJ','JOD','MGS',
+  'WEG','CTZ','GUR','SOS','MNZ','BIB','GUU','MGR','MGS','MUR','RJC','RJM','MON',
+  'RJN','RJE','SCL','BOG','CET','GAD','GUX','HMM','JDP','MKG','OAL','RJB','ROL',
+  'SCO','SKX','SPU','SRG','TRI','TTR','UCA','USB')) x <- "DEF" # "Demersal_fish"  # FAO sp code
 
-  if(x %in% c('ABR','HLT','KRS','MUS','KNK','KMS','SNE','MOL','CLR','CLH','CLQ','CLS','CLT','COC','SCB','SCC','ISC','SCA','SCX','OYA','WHX','PER','WHE','QSC','VSC','MYV','VEV','GKL','SCE','OYF','OYG')) x <- "MOL" # "Molluscs"  # FAO sp code
+  if(x %in% c('ABR','HLT','KRS','MUS','KNK','KMS','SNE','MOL','CLR','CLH','CLQ',
+  'CLS','CLT','COC','SCB','SCC','ISC','SCA','SCX','OYA','WHX','PER','WHE','QSC',
+  'VSC','MYV','VEV','GKL','SCE','OYF','OYG','VNR','CLJ','ULV','TPS','CTG','CLX',
+  'COZ','KFA','KLK','SJA','TWL','KDF','OYX')) x <- "MOL" # "Molluscs"  # FAO sp code
 
-  if(x %in% c('ELE','STU')) x <- "CAT" # "Catadromus_species"   # FAO sp code
+  if(x %in% c('STU')) x <- "CAT" # "Catadromus_species"   # FAO sp code
 
-  if (x %in% c('ANE','HER','SPR','MAC','HKE','SAU','ANB','SSM','MHA','BUT','CAP','PIL','JAX','SHG','ARU','ARY','MAS','GAR','MUL','WHB','NOP','HOM')) x <- "SPF" # "Small_pelagic_fish"  # FAO sp code
+  if (x %in% c('ANE','HER','SPR','MAC','HKE','SAU','ANB','SSM','MHA','BUT',
+  'CAP','PIL','JAX','SHG','ARU','ARY','MAS','GAR','MUL','WHB','NOP','HOM')) x <- "SPF" # "Small_pelagic_fish"  # FAO sp code
 
-  if (x %in% c('BLU','CVJ','FRI','KGM','SSM','SAI','WHM','BUM','SWO','ALB','BON','LTA','BET','BFT','SKJ','YFT','POR','DGS','GUQ','DGX','SRX','SKA','SRX','TUN','GUQ','SBG')) x <- "LPF" # "Large_pelagic_fish"  # FAO sp code
+  if (x %in% c('BLU','CVJ','FRI','KGM','SSM','SAI','WHM','BUM','SWO','ALB',
+  'BON','LTA','BET','BFT','SKJ','YFT','POR','DGS','GUQ','DGX','SRX','SKA','SRX',
+  'TUN','GUQ','SBG')) x <- "LPF" # "Large_pelagic_fish"  # FAO sp code
 
-  if (x %in% c('CHR','SAL','APU','TRS','TRR','SLZ','TLV','PLN','VIV','LAR','STU','SME','TRO','SHG','SHD','PLN','LAU')) x <- "ANA" # "Anadromous_species"
+  if (x %in% c('CHR','SAL','APU','TRS','TRR','SLZ','TLV','PLN','VIV','LAR',
+  'STU','SME','TRO','SHG','SHD','PLN','LAU')) x <- "ANA" # "Anadromous_species"
 
-  if (x %in% c('FBM','FPE','FPI','PLN','FBR','FCP','FTE','BRB','FBR','FBU','FCC','FCP','FPP','FRO','SAR','ELP','FFX')) x <- "FWS" # "Freshwater_species"
+  if (x %in% c('FBM','FPE','FPI','PLN','FBR','FCP','FTE','BRB','FBR','FBU',
+  'FCC','FCP','FPP','FRO','SAR','ELP','FFX')) x <- "FWS" # "Freshwater_species"
 
-  if(x %in% c('SQL','SQI','SQU','OCC','CTC','SQR','IAX','SQZ','OMZ','CTL')) x <- "CEP" # "Cephalopods"
+  if(x %in% c('SQL','SQI','SQU','OCC','CTC','SQR','IAX','SQZ','OMZ','CTL','IAR',
+  'OCT')) x <- "CEP" # "Cephalopods"
   
   if(x %in% c('HYD','ORY')) x <- "DWS" # "Deep_Water_Fish"
+  
+  if (x %in% c('ELE','EL1'))  x <- "EEL" # "Glass eel"   # FAO sp code
+  
+  if (x %in% c('LAH','LQD','SWB'))  x <- "SWX" # "Algae"   # FAO sp code
 
   if(x %in% c('MZZ','OTH','FRF','YYY','ZZZ')) x <- "OTH" # "Other"  # FAO sp code
   
