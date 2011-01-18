d884c4b926c4dd0c67192a48b8f55caefcdf0e23
Niels.Hintzen <niels.hintzen@gmail.com>
2011-01-18 12:09:51 +0000

    updated description file added if statement to test if SI_DATIM existed in sortTacsat

--- vmstools/R/sortTacsat.r
+++ vmstools/R/sortTacsat.r
@@ -1,12 +1,11 @@
 `sortTacsat` <-
 function(tacsat){
 
-  #Load a dataframe sorter library      
-library(doBy)
-SI_DATIM  <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M:%S")
-tacsat    <- cbind(tacsat,SI_DATIM)
+if(!"SI_DATIM" %in% colnames(tacsat)) tacsat$SI_DATIM  <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M:%S")
+
   #Sort the tacsat data first by ship, then by date
 tacsat <- orderBy(~VE_REF+SI_DATIM,data=tacsat)
-return(tacsat[,-grep("SI_DATIM",colnames(tacsat))])}
+
+return(tacsat)}
 
                                                 
