f56a7db687e105243b4cdb4c01d6ff93d274da9c
Niels.Hintzen <niels.hintzen@gmail.com>
2011-01-18 15:10:34 +0000

    additional fixes in interpolation2Tacsat.r additional bug fixes in splitAmongpings to secure that if you don't have trips or no-trips it won't fail

--- vmstools/R/interpolation2Tacsat.r
+++ vmstools/R/interpolation2Tacsat.r
@@ -4,6 +4,7 @@ interpolation2Tacsat <- function(interpolation,tacsat,npoints=10){
 # This function takes the list of tracks output by interpolateTacsat and converts them back to tacsat format.
 # The npoints argument is the optional number of points between each 'real' position.
 
+if(!"SI_DATIM" %in% colnames(tacsat)) tacsat$SI_DATIM  <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M")
 interpolationEQ <- equalDistance(interpolation,npoints)  #Divide points equally along interpolated track (default is 10).
 
 res <- lapply(interpolationEQ,function(x){
@@ -12,10 +13,12 @@ res <- lapply(interpolationEQ,function(x){
                                   x$VE_COU                <- rep(tacsat$VE_COU[idx[1]],nrow(x))
                                   x$VE_REF                <- rep(tacsat$VE_REF[idx[1]],nrow(x))
                                   x$FT_REF                <- rep(tacsat$FT_REF[idx[1]],nrow(x))
-                                  x$SI_DATE               <- tacsat$SI_DATE[idx[1]]
-                                  x$SI_TIME               <- tacsat$SI_TIME[idx[1]]
                                   x$SI_DATIM              <- tacsat$SI_DATIM[idx[1]]
                                   x$SI_DATIM[-c(1:2)]     <- as.POSIXct(cumsum(rep(difftime(tacsat$SI_DATIM[idx[2]],tacsat$SI_DATIM[idx[1]],unit="secs")/(nrow(x)-2),nrow(x)-2))+tacsat$SI_DATIM[idx[1]],tz="GMT",format = "%d/%m/%Y  %H:%M")
+                                  x$SI_DATE               <- format(x$SI_DATIM,format="%d/%m/%Y")
+                                  timeNotation            <- ifelse(length(unlist(strsplit(tacsat$SI_TIME[1],":")))>2,"secs","mins")
+                                  if(timeNotation == "secs") x$SI_TIME  <- format(x$SI_DATIM,format="%H:%M:%S")
+                                  if(timeNotation == "mins") x$SI_TIME  <- format(x$SI_DATIM,format="%H:%M")
                                   x$SI_SP                 <- mean(tacsat$SI_SP[idx[1]],tacsat$SI_SP[idx[2]],na.rm=T)
                                   x$SI_HE                 <- NA;
                                   x$SI_HE[-c(1,nrow(x))]  <- bearing(x$SI_LONG[3:nrow(x)],x$SI_LATI[3:nrow(x)],x$SI_LONG[2:(nrow(x)-1)],x$SI_LATI[2:(nrow(x)-1)])
