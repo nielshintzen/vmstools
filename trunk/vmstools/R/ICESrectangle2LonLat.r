e280affd226152f3ff2f055941ff66e6f779376e
niels.hintzen@gmail.com <niels.hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-01-24 08:26:27 +0000

    -got destFromBearing working to output a spatialPolygon class file -got rid of LE_EFF and LE_EFF_VMS as checks in formatEflalo -new function to add a gearwidth to an interpolated track and to be able to plot it -new function to find the point where you end given a starting position, a bearing and a distance to travel -new function to do the ICES-rectangle to lon-lat position conversion (this one includes more possibilities)

---
+++ trunk/vmstools/R/ICESrectangle2LonLat.r
@@ -0,0 +1,20 @@
+ICESrectangle2LonLat <-
+function (statsq) 
+{
+    part1 <- substr(statsq, 1, 2)
+    part2 <- substr(statsq, 3, 4)
+    labels <- 0:90
+    latlabels <- ifelse(labels < 10, paste("0", labels, sep = ""), 
+        as.character(labels))
+    latvalues <- seq(35.5, 80.5, 0.5) + 0.25
+    lonlabels <- paste(rep(LETTERS[2:8], rep(10, 7)), rep(0:9, 
+        7), sep = "")
+    lonvalues <- (-40:29) + 0.5
+    indx <- match(part1, latlabels)
+    lat <- latvalues[indx]
+    indx <- match(part2, lonlabels)
+    lon <- lonvalues[indx]
+    if (any(is.na(lat)) | any(is.na(lon))) 
+        warning("Some stat squares have not been recognised.")
+    return(data.frame(lat, lon))
+}
