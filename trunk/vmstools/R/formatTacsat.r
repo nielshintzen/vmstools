2879c0a3bd9300eea3d44f606a6b182268524594
niels.hintzen@gmail.com <niels.hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-01-27 12:18:09 +0000

    updated helpfiles + added checks for formatTacsat

--- trunk/vmstools/R/formatTacsat.r
+++ trunk/vmstools/R/formatTacsat.r
@@ -6,9 +6,9 @@ formatTacsat <- function(x){
   x$SI_TIME   <- ac(x$SI_TIME)
   x$SI_SP     <- an(x$SI_SP)
   x$SI_HE     <- an(x$SI_HE)
-  if("SI_HARB" in colnames(x))  x$SI_HARB   <- ac(x$SI_HARB)
-  if("SI_STATE" in colnames(x)) x$SI_STATE  <- ac(x$SI_STATE)
-  if("SI_FT" in colnames(x))    x$SI_FT     <- ac(x$SI_FT)
+  if("SI_HARB" %in% colnames(x))  x$SI_HARB   <- ac(x$SI_HARB)
+  if("SI_STATE" %in% colnames(x)) x$SI_STATE  <- ac(x$SI_STATE)
+  if("SI_FT" %in% colnames(x))    x$SI_FT     <- ac(x$SI_FT)
   #Get rid of NAs in the long and lats
   x <- x[!is.na(x$SI_LATI),]
   x <- x[!is.na(x$SI_LONG),]
