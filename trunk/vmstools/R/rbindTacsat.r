a27c767f11d84e727e4152c28b76e2ba8c229208
Niels.Hintzen@gmail.com <Niels.Hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2011-07-19 15:23:54 +0000

    function to merge eflalo or tacsat files together (no help file yet)
    commented out call to plotHabitatMap because it will fail building

---
+++ trunk/vmstools/R/rbindTacsat.r
@@ -0,0 +1,23 @@
+rbindTacsat <- function(set1,set2){
+  cln1  <- colnames(set1)
+  cln2  <- colnames(set2)
+  if(any(duplicated(cln1)==T) || any(duplicated(cln2)==T)) stop("Duplicate column names in datasets")
+  idx1  <- which(is.na(pmatch(cln1,cln2))==T)
+  idx2  <- which(is.na(pmatch(cln2,cln1))==T)
+  
+  if(length(idx1)>0){
+    for(i in idx1) set2 <- cbind(set2,NA)
+    colnames(set2) <- c(cln2,cln1[idx1])}
+  if(length(idx2)>0){
+    for(i in idx2) set1 <- cbind(set1,NA)
+    colnames(set1) <- c(cln1,cln2[idx2])}
+  cln1  <- colnames(set1)
+  cln2  <- colnames(set2)
+  mtch  <- pmatch(cln1,cln2)
+  if(any(is.na(mtch))==T) stop("Cannot find nor create all matching column names")
+  set3  <- rbind(set1,set2[,cln2[mtch]])
+return(set3)}
+  
+
+
+
