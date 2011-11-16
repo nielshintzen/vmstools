7f4225cfe2f5d84cf4ad24d74e26d188e8763c11
Niels.Hintzen <niels.hintzen@gmail.com>
2011-11-16 08:25:18 +0000

    clipObs2Tacsat: Added units of time interval intervalTacsat: Rewrite, now possible to take average of heading towards and coming from position mergeEflalo2Tacsat: check for SI_DATIM etc calculateSpeed: New function to calculate speed of dataset

---
+++ vmstools/R/calculateSpeed.r
@@ -0,0 +1,104 @@
+calculateSpeed <- function(tacsat,level="vessel",weight=c(1,1),fill.na=F){
+
+  if(length(weight) != 2) stop("weight must be specified as a length 2 numeric vector")
+  weight <- weight / sum(weight,na.rm=T)
+
+  #- Add interval between succeeding points
+  tacsatp <- intervalTacsat(tacsat,level=level,weight=weight,fill.na=fill.na)
+
+  if(!"SI_DATIM" %in% colnames(tacsatp)) tacsatp$SI_DATIM  <- as.POSIXct(paste(tacsatp$SI_DATE,  tacsatp$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M")
+  if(level=="trip"){
+    if(is.null(tacsatp$FT_REF)==T) stop("no tripnumber available to merge on trip level")
+    sptacsat          <- split(tacsatp,tacsatp$VE_REF)
+    tacsatp$SI_SPCA   <- unlist(lapply(sptacsat,function(x){
+
+      FT_REF  <- as.factor(x$FT_REF);
+      res     <- by(x,FT_REF,
+                  function(y){
+                    if(nrow(y)>1){
+                      interval        <- y$INTV
+                      distance_xmin1  <- c(NA,distance(y$SI_LONG[2:nrow(y)],    y$SI_LATI[2:nrow(y)],
+                                                       y$SI_LONG[1:(nrow(y)-1)],y$SI_LATI[1:(nrow(y)-1)]))
+                      distance_xplus1 <- c(distance_xmin1[-1],NA)
+                      if(any(weight == 0)){
+                        if(weight[1] == 0) SI_SPCA       <- distance_xplus1 / (interval/60)
+                        if(weight[2] == 0) SI_SPCA       <- distance_xmin1 / (interval/60)
+                      } else {
+                          difftime_xmin1                 <- intervalTacsat(y,level="trip",weight=c(1,0),fill.na=fill.na)$INTV
+                          difftime_xplus1                <- intervalTacsat(y,level="trip",weight=c(0,1),fill.na=fill.na)$INTV
+                          SI_SPCA                        <- 2*(2* ((distance_xmin1 * weight[1]) / (difftime_xmin1/60)) * (distance_xplus1 * weight[2] / (difftime_xplus1/60)) /
+                                                                 (((distance_xmin1 * weight[1]) / (difftime_xmin1/60)) + (distance_xplus1 * weight[2] / (difftime_xplus1/60))))
+                        }
+                      #- If INTV equals NA, then check if there are other possibilities to calculate the interval rate based on a different
+                      #   weight setting.
+                      if(fill.na==T){
+                        idx                               <- which(is.na(SI_SPCA)==T)
+                        if(weight[1] == 0 | weight[2] == 0){
+                          speeds                          <- cbind((distance_xmin1[idx]   * weight[1]) / (interval[idx]/60),
+                                                                   (distance_xplus1[idx]  * weight[2]) / (interval[idx]/60))
+                        } else {
+                            speeds                        <- cbind((distance_xmin1[idx]   * weight[1]) / (difftime_xmin1[idx] /60),
+                                                                   (distance_xplus1[idx]  * weight[2]) / (difftime_xplus1[idx]/60))
+                          }
+                        #- Use of 0.5 if ifelse = NA, as counter to 2*...
+                        SI_SPCA[idx]                      <- 2*(2* ifelse(is.na(speeds[,1])==T,0.5,speeds[,1]) * ifelse(is.na(speeds[,2])==T,0.5,speeds[,2]) /
+                                                                  (ifelse(is.na(speeds[,1])==T,0,speeds[,1])   + ifelse(is.na(speeds[,2])==T,0,speeds[,2])))
+                        SI_SPCA[idx][which(SI_SPCA[idx]==0)]<- NA
+                      }
+
+                      return(SI_SPCA)
+                    } else {
+                        return(NA)
+                      }
+                  })
+                                          return(unsplit(res,FT_REF))}))
+    tacsatp$SI_SPCA[which(tacsatp$FT_REF == "0")] <- NA
+
+  }
+  if(level=="vessel"){
+    #- Take the interval for weights c(0,1) and c(1,0) direct from tacsat
+    interval                            <- tacsatp$INTV
+    distance_xmin1                      <- c(NA,distance(tacsatp$SI_LONG[2:nrow(tacsatp)],     tacsatp$SI_LATI[2:nrow(tacsatp)],
+                                                         tacsatp$SI_LONG[1:(nrow(tacsatp)-1)], tacsatp$SI_LATI[1:(nrow(tacsatp)-1)]))
+    distance_xplus1                     <- c(distance_xmin1[-1],NA)
+    #- Recalculate the interval rate for either outer averages
+    difftime_xmin1                      <- intervalTacsat(tacsatp,level="fleet",weight=c(1,0),fill.na=fill.na)$INTV
+    difftime_xplus1                     <- intervalTacsat(tacsatp,level="fleet",weight=c(0,1),fill.na=fill.na)$INTV
+    if(any(weight == 0)){
+      if(weight[1] == 0) SI_SPCA        <- distance_xplus1 / (interval/60)
+      if(weight[2] == 0) SI_SPCA        <- distance_xmin1 / (interval/60)
+    } else {
+        SI_SPCA                         <- 2*(2* ((distance_xmin1 * weight[1]) / (difftime_xmin1/60)) * (distance_xplus1 * weight[2] / (difftime_xplus1/60)) /
+                                                (((distance_xmin1 * weight[1]) / (difftime_xmin1/60)) + (distance_xplus1 * weight[2] / (difftime_xplus1/60))))
+      }
+    #- If INTV equals NA, then check if there are other possibilities to calculate the interval rate based on a different
+    #   weight setting.
+    if(fill.na==T){
+      idx                               <- which(is.na(SI_SPCA)==T)
+      if(weight[1] == 0 | weight[2] == 0){
+        speeds                          <- cbind((distance_xmin1[idx]   * weight[1]) / (interval[idx]/60),
+                                                 (distance_xplus1[idx]  * weight[2]) / (interval[idx]/60))
+      } else {
+          speeds                        <- cbind((distance_xmin1[idx]   * weight[1]) / (difftime_xmin1[idx] /60),
+                                                 (distance_xplus1[idx]  * weight[2]) / (difftime_xplus1[idx]/60))
+        }
+      #- Use of 0.5 if ifelse = NA, as counter to 2*...
+      SI_SPCA[idx]                      <- 2*(2* ifelse(is.na(speeds[,1])==T,0.5,speeds[,1]) * ifelse(is.na(speeds[,2])==T,0.5,speeds[,2]) /
+                                                (ifelse(is.na(speeds[,1])==T,0,speeds[,1])   + ifelse(is.na(speeds[,2])==T,0,speeds[,2])))
+      SI_SPCA[idx][which(SI_SPCA[idx]==0)]<- NA
+    }
+    tacsatp$SI_SPCA                 <- SI_SPCA
+
+    vessels                         <- unique(tacsatp$VE_REF)
+    first.vessels                   <- unlist(lapply(as.list(vessels),function(x){which(tacsatp$VE_REF==x)[1]}))
+    last.vessels                    <- unlist(lapply(as.list(vessels),function(x){rev(which(tacsatp$VE_REF==x))[1]}))
+    if(weight[1] != 0) tacsatp$SI_SPCA[first.vessels]      <- NA
+    if(weight[2] != 0) tacsatp$SI_SPCA[last.vessels]       <- NA
+    if(fill.na==T) tacsatp$SI_SPCA[first.vessels]          <- distance_xplus1[first.vessels] / (interval[first.vessels]/60)
+    if(fill.na==T) tacsatp$SI_SPCA[last.vessels]           <- distance_xmin1[last.vessels] / (interval[last.vessels]/60)
+
+  }
+  idx <- which(colnames(tacsatp) %in% c("INTV"))
+  #- Convert from km/h to knots / nautical miles an hour
+  tacsatp$SI_SPCA     <- tacsatp$SI_SPCA / 1.852
+return(if(length(idx)>0){tacsatp[,-idx]}else{tacsatp})}
--- vmstools/R/intervalTacsat.r
+++ vmstools/R/intervalTacsat.r
@@ -1,16 +1,72 @@
-intervalTacsat <- function(tacsat,level="trip"){
+intervalTacsat <- function(tacsat,level="trip",weight=c(1,0),fill.na=F){
+
+                    if(length(weight) != 2) stop("weight must be specified as a length 2 numeric vector")
+                    weight <- weight / sum(weight,na.rm=T)
+
+                    #- First sort the tacsat dataset
+                    tacsat <- sortTacsat(tacsat)
+
+                    #- Add date-time stamp
                     if(!"SI_DATIM" %in% colnames(tacsat)) tacsat$SI_DATIM  <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M")
+
+                    #- If a trip level is specified, the interval rate can be calculated by trip (to make sure that no long
+                    #   interval rates between trips occur in comparison to by level="vessel"
                     if(level=="trip"){
                       if(is.null(tacsat$FT_REF)==T) stop("no tripnumber available to merge on trip level")
                       sptacsat      <- split(tacsat,tacsat$VE_REF)
-                      tacsat$INTV   <- unlist(lapply(sptacsat,function(x){FT_REF  <- as.factor(x$FT_REF);
-                                                                        res     <- by(x,FT_REF,function(y){ if(nrow(y)>1){ return(c(NA,difftime(y$SI_DATIM[2:nrow(y)],y$SI_DATIM[1:(nrow(y)-1)],units="mins")))}else{return(NA)}})
-                                                            return(unlist(res))}))
+                      tacsat$INTV   <- unlist(lapply(sptacsat,function(x){
+
+                        FT_REF  <- as.factor(x$FT_REF);
+                        res     <- by(x,FT_REF,
+                                    function(y){
+                                      if(nrow(y)>1){
+                                        difftime_xmin1  <- c(NA,difftime(y$SI_DATIM[2:nrow(y)],y$SI_DATIM[1:(nrow(y)-1)],units="mins"))
+                                        difftime_xplus1 <- c(difftime_xmin1[-1],NA)
+                                        if(any(weight == 0)){
+                                          if(weight[1] == 0) INTV       <- difftime_xplus1
+                                          if(weight[2] == 0) INTV       <- difftime_xmin1
+                                        } else {             INTV       <- rowSums(cbind(difftime_xmin1*weight[1],difftime_xplus1*weight[2]))
+                                          }
+                                        #- If INTV equals NA, then check if there are other possibilities to calculate the interval rate based on a different
+                                        #   weight setting.
+                                        if(fill.na==T){
+                                          idx                           <- which(is.na(INTV)==T)
+                                          INTV[idx]                     <- rowSums(cbind(difftime_xmin1[idx],difftime_xplus1[idx]),na.rm=T)
+                                          INTV[idx][which(INTV[idx]==0)]<- NA
+                                        }
+
+                                        return(INTV)
+                                      } else {
+                                          return(NA)
+                                        }
+                                    })
+                                                            return(unsplit(res,FT_REF))}))
+                      tacsat$INTV[which(tacsat$FT_REF == "0")] <- NA
                     }
+                    #- If no trip level is specified, the other option is to calculate interval rate by vessel
                     if(level=="vessel"){
-                      tacsat$INTV[2:nrow(tacsat)]  <- difftime(tacsat$SI_DATIM[2:nrow(tacsat)],tacsat$SI_DATIM[1:(nrow(tacsat)-1)],units="mins")
+                      difftime_xmin1                  <- c(NA,difftime(tacsat$SI_DATIM[2:nrow(tacsat)],tacsat$SI_DATIM[1:(nrow(tacsat)-1)],units="mins"))
+                      difftime_xplus1                 <- c(difftime_xmin1[-1],NA)
+                      if(any(weight == 0)){
+                        if(weight[1] == 0) INTV       <- difftime_xplus1
+                        if(weight[2] == 0) INTV       <- difftime_xmin1
+                      } else {             INTV       <- rowSums(cbind(difftime_xmin1*weight[1],difftime_xplus1*weight[2]))
+                        }
+                      #- If INTV equals NA, then check if there are other possibilities to calculate the interval rate based on a different
+                      #   weight setting.
+                      if(fill.na==T){
+                        idx                           <- which(is.na(INTV)==T)
+                        INTV[idx]                     <- rowSums(cbind(difftime_xmin1[idx],difftime_xplus1[idx]),na.rm=T)
+                        INTV[idx][which(INTV[idx]==0)]<- NA
+                      }
+                      tacsat$INTV                     <- INTV
+
                       vessels                         <- unique(tacsat$VE_REF)
                       first.vessels                   <- unlist(lapply(as.list(vessels),function(x){which(tacsat$VE_REF==x)[1]}))
-                      tacsat$INTV[first.vessels]    <- NA
+                      last.vessels                    <- unlist(lapply(as.list(vessels),function(x){rev(which(tacsat$VE_REF==x))[1]}))
+                      if(weight[1] != 0) tacsat$INTV[first.vessels]      <- NA
+                      if(weight[2] != 0) tacsat$INTV[last.vessels]       <- NA
+                      if(fill.na==T) tacsat$INTV[first.vessels]  <- difftime_xplus1[first.vessels]
+                      if(fill.na==T) tacsat$INTV[last.vessels]   <- difftime_xmin1[last.vessels]
                     }
                   return(tacsat)}
