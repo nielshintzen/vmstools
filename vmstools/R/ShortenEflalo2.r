427418b6276389ae55378acbb13d12449bb20b71
Niels.Hintzen <niels.hintzen@gmail.com>
2011-12-29 11:27:55 +0000

    alternative way to rename ShortenEflalo2

--- vmstools/R/ShortenEflalo2.r
+++
@@ -1,9 +0,0 @@
-shortenEflalo2 <- function(data=eflalo2,which.species = c("ANE","BIB","BLL","COD","DAB","HAD","HER","MAC","NEP","PLE","SOL","WHG") )
-{
-#Eflalo is an unwieldy format so this is handy if you want to select a few species
-dn <- dimnames(data)[[2]]
-yp <- NULL
-for(ss in which.species){yp <- c(yp,grep(ss,dn)) }
-short.eflalo2 <- data[,c(1:26,yp)]
-short.eflalo2
-}
