cbbab1ea927acacb7246273fe2569a190297d2f2
Niels.Hintzen@gmail.com <Niels.Hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2012-03-29 14:36:58 +0000

    update bug fixes. No LE_UNIT is eflalo by default, so check createGrid did not create proper bounds when resx and resy were set to small values

--- trunk/vmstools/R/createGrid.r
+++ trunk/vmstools/R/createGrid.r
@@ -7,10 +7,11 @@ function(xrange
                              ){
                 
                 library(sp)             
-                roundDigit  <- max(getndp(resx),getndp(resy),na.rm=T)
-                xborder     <- seq(floor(sort(xrange)[1]),ceiling(sort(xrange)[2]),resx)
-                yborder     <- seq(floor(sort(yrange)[1]),ceiling(sort(yrange)[2]),resy)
+                roundDigit  <- max(getndp(resx),getndp(resy),na.rm=T)-1
+                xborder     <- seq(floor(sort(xrange)[1]*(10^roundDigit))/(10^roundDigit),ceiling(sort(xrange)[2]*(10^roundDigit))/(10^roundDigit),resx)
+                yborder     <- seq(floor(sort(yrange)[1]*(10^roundDigit))/(10^roundDigit),ceiling(sort(yrange)[2]*(10^roundDigit))/(10^roundDigit),resy)
                 
+                if(xrange[1]<xborder[1] | xrange[2]>rev(xborder)[1] | yrange[1]<yborder[1] | yrange[2]>rev(yborder)[1]) stop("Grid range smaller than specified bounds (bug!)")
                 grid        <- GridTopology(c(xborder[1],yborder[1]),c(resx,resy),c(length(xborder),length(yborder)))
                 if(type=="SpatialGrid"){
                   grid      <- SpatialGrid(grid=grid);
