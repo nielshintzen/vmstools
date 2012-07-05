8a5c543e0a24605dc3277279e3b236f9b09e1f08
Niels.Hintzen@gmail.com <Niels.Hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2012-07-05 14:02:58 +0000

    proper check for conversion of factors to numerics

--- trunk/vmstools/R/formatTacsat.r
+++ trunk/vmstools/R/formatTacsat.r
@@ -1,16 +1,17 @@
 formatTacsat <- function(x){
   x$VE_REF    <- ac(x$VE_REF)
-  x$SI_LATI   <- an(x$SI_LATI)
-  x$SI_LONG   <- an(x$SI_LONG)
+  x$SI_LATI   <- anf(x$SI_LATI)
+  x$SI_LONG   <- anf(x$SI_LONG)
   x$SI_DATE   <- ac(x$SI_DATE)
   x$SI_TIME   <- ac(x$SI_TIME)
-  x$SI_SP     <- an(x$SI_SP)
-  x$SI_HE     <- an(x$SI_HE)
+  x$SI_SP     <- anf(x$SI_SP)
+  x$SI_HE     <- anf(x$SI_HE)
   if("SI_HARB" %in% colnames(x))  x$SI_HARB   <- ac(x$SI_HARB)
   if("SI_STATE" %in% colnames(x)) x$SI_STATE  <- ac(x$SI_STATE)
   if("SI_FT" %in% colnames(x))    x$SI_FT     <- ac(x$SI_FT)
   #Get rid of NAs in the long and lats
   x <- x[!is.na(x$SI_LATI),]
   x <- x[!is.na(x$SI_LONG),]
+  warnings("Those records where longitude and latitude were NA have been removed")
   return(x)
 }
