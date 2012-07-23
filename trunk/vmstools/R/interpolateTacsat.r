5580f88e0b36c27bc6761e416fd5b22d94fb7c90
kg.hamon@gmail.com <kg.hamon@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2012-07-23 13:25:46 +0000

    removed the copying of the tacsat object at the beginning

--- trunk/vmstools/R/interpolateTacsat.r
+++ trunk/vmstools/R/interpolateTacsat.r
@@ -8,8 +8,7 @@ function(tacsat                          #VMS datapoints
                               ,headingAdjustment=0
                               ){
 
-VMS. <- tacsat
-if(!"SI_DATIM" %in% colnames(VMS.)) VMS.$SI_DATIM     <- as.POSIXct(paste(VMS.$SI_DATE,  VMS.$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M")
+if(!"SI_DATIM" %in% colnames(tacsat)) tacsat$SI_DATIM     <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M")
                               
   #Start interpolating the data
 if(!method %in% c("cHs","SL"))  stop("method selected that does not exist")
@@ -18,27 +17,27 @@ if(!method %in% c("cHs","SL"))  stop("method selected that does not exist")
 returnInterpolations <- list()
 
   #Start iterating over succeeding points
-for(iStep in 1:(dim(VMS.)[1]-1)){
+for(iStep in 1:(dim(tacsat)[1]-1)){
   if(iStep == 1){
     iSuccess    <- 0
     endDataSet  <- 0
     startVMS    <- 1
-    ship        <- VMS.$VE_REF[startVMS]
+    ship        <- tacsat$VE_REF[startVMS]
   } else {
       if(is.na(endVMS)==T) endVMS <- startVMS + 1
       startVMS <- endVMS
       #-Check if the end of the dataset is reached
-      if(endDataSet == 1 & rev(unique(VMS.$VE_REF))[1] != ship){
-        startVMS  <- which(VMS.$VE_REF == unique(VMS.$VE_REF)[which(unique(VMS.$VE_REF)==ship)+1])[1]
-        ship      <- VMS.$VE_REF[startVMS]
+      if(endDataSet == 1 & rev(unique(tacsat$VE_REF))[1] != ship){
+        startVMS  <- which(tacsat$VE_REF == unique(tacsat$VE_REF)[which(unique(tacsat$VE_REF)==ship)+1])[1]
+        ship      <- tacsat$VE_REF[startVMS]
         endDataSet<- 0
       }
-      if(endDataSet == 1 & rev(unique(VMS.$VE_REF))[1] == ship) endDataSet <- 2 #Final end of dataset
+      if(endDataSet == 1 & rev(unique(tacsat$VE_REF))[1] == ship) endDataSet <- 2 #Final end of dataset
     }
 
   #if end of dataset is not reached, try to find succeeding point
   if(endDataSet != 2){
-    result      <- findEndTacsat(VMS.,startVMS,interval,margin)
+    result      <- findEndTacsat(tacsat,startVMS,interval,margin)
     endVMS      <- result[1]
     endDataSet  <- result[2]
     if(is.na(endVMS)==T) int <- 0  #No interpolation possible
@@ -59,26 +58,26 @@ for(iStep in 1:(dim(VMS.)[1]-1)){
       F01 <- -2*t^3+3*t^2
       F11 <- t^3-t^2          
 
-      if (is.na(VMS.[startVMS,"SI_HE"])=="TRUE") VMS.[startVMS,"SI_HE"] <- 0
-      if (is.na(VMS.[endVMS,  "SI_HE"])=="TRUE") VMS.[endVMS,  "SI_HE"] <- 0
+      if (is.na(tacsat[startVMS,"SI_HE"])=="TRUE") tacsat[startVMS,"SI_HE"] <- 0
+      if (is.na(tacsat[endVMS,  "SI_HE"])=="TRUE") tacsat[endVMS,  "SI_HE"] <- 0
       
         #Heading at begin point in degrees
-      Hx0 <- sin(VMS.[startVMS,"SI_HE"]/(180/pi))
-      Hy0 <- cos(VMS.[startVMS,"SI_HE"]/(180/pi))
+      Hx0 <- sin(tacsat[startVMS,"SI_HE"]/(180/pi))
+      Hy0 <- cos(tacsat[startVMS,"SI_HE"]/(180/pi))
         #Heading at end point in degrees
-      Hx1 <- sin(VMS.[endVMS-headingAdjustment,"SI_HE"]/(180/pi))
-      Hy1 <- cos(VMS.[endVMS-headingAdjustment,"SI_HE"]/(180/pi))
+      Hx1 <- sin(tacsat[endVMS-headingAdjustment,"SI_HE"]/(180/pi))
+      Hy1 <- cos(tacsat[endVMS-headingAdjustment,"SI_HE"]/(180/pi))
       
-      Mx0 <- VMS.[startVMS, "SI_LONG"]
-      Mx1 <- VMS.[endVMS,   "SI_LONG"]
-      My0 <- VMS.[startVMS, "SI_LATI"]
-      My1 <- VMS.[endVMS,   "SI_LATI"]
+      Mx0 <- tacsat[startVMS, "SI_LONG"]
+      Mx1 <- tacsat[endVMS,   "SI_LONG"]
+      My0 <- tacsat[startVMS, "SI_LATI"]
+      My1 <- tacsat[endVMS,   "SI_LATI"]
 
         #Corrected for longitude lattitude effect
-      Hx0 <- Hx0 * params$fm * VMS.[startVMS,"SI_SP"] /((params$st[2]-params$st[1])/2+params$st[1])
-      Hx1 <- Hx1 * params$fm * VMS.[endVMS,"SI_SP"]   /((params$st[2]-params$st[1])/2+params$st[1])
-      Hy0 <- Hy0 * params$fm * lonLatRatio(VMS.[c(startVMS,endVMS),"SI_LONG"],VMS.[c(startVMS,endVMS),"SI_LATI"])[1] * VMS.[startVMS,"SI_SP"]/((params$st[2]-params$st[1])/2+params$st[1])
-      Hy1 <- Hy1 * params$fm * lonLatRatio(VMS.[c(startVMS,endVMS),"SI_LONG"],VMS.[c(startVMS,endVMS),"SI_LATI"])[2] * VMS.[endVMS,"SI_SP"]/((params$st[2]-params$st[1])  /2+params$st[1])
+      Hx0 <- Hx0 * params$fm * tacsat[startVMS,"SI_SP"] /((params$st[2]-params$st[1])/2+params$st[1])
+      Hx1 <- Hx1 * params$fm * tacsat[endVMS,"SI_SP"]   /((params$st[2]-params$st[1])/2+params$st[1])
+      Hy0 <- Hy0 * params$fm * lonLatRatio(tacsat[c(startVMS,endVMS),"SI_LONG"],tacsat[c(startVMS,endVMS),"SI_LATI"])[1] * tacsat[startVMS,"SI_SP"]/((params$st[2]-params$st[1])/2+params$st[1])
+      Hy1 <- Hy1 * params$fm * lonLatRatio(tacsat[c(startVMS,endVMS),"SI_LONG"],tacsat[c(startVMS,endVMS),"SI_LATI"])[2] * tacsat[endVMS,"SI_SP"]/((params$st[2]-params$st[1])  /2+params$st[1])
 
         #Finalizing the interpolation based on cHs
       fx <- numeric()
@@ -93,8 +92,8 @@ for(iStep in 1:(dim(VMS.)[1]-1)){
     
       #Interpolate according to a straight line
     if(method == "SL" & int == 1){
-      fx <- seq(VMS.$SI_LONG[startVMS],VMS.$SI_LONG[endVMS],length.out=res)
-      fy <- seq(VMS.$SI_LATI[startVMS],VMS.$SI_LATI[endVMS],length.out=res)
+      fx <- seq(tacsat$SI_LONG[startVMS],tacsat$SI_LONG[endVMS],length.out=res)
+      fy <- seq(tacsat$SI_LATI[startVMS],tacsat$SI_LATI[endVMS],length.out=res)
       
         #Add one to list of successful interpolations
       iSuccess <- iSuccess + 1
