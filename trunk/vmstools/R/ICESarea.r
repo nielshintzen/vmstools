e9ab5e2b2eea8ea103fda574fcbabb732c8c01a3
Niels.Hintzen@gmail.com <Niels.Hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2012-12-06 12:18:05 +0000

    updates in functions and bug fixes and addition of summarize functions

--- trunk/vmstools/R/ICESarea.r
+++ trunk/vmstools/R/ICESarea.r
@@ -1,22 +1,25 @@
 ICESarea <- function(tacsat,areas,proj4string=NULL){
       if(class(areas) != "SpatialPolygons") stop("'areas' must be specified as class 'SpatialPolygons'")
 
-      totres      <- rep(0,length(tacsat$SI_LONG))
+      #filter NA values
+      NAS         <- which(is.na(tacsat$SI_LONG)==F & is.na(tacsat$SI_LATI)==F)
+
+      totres      <- rep(NA,length(tacsat$SI_LONG))
       nms         <- unlist(lapply(areas@polygons,function(x){return(x@ID)}))
 
       if(is.null(proj4string)==T){
         #No projection string used
         if(is.na(proj4string(areas))==F) stop("Projection defined for areas, use proj4string argument in function")
-        spPoint           <- SpatialPoints(data.frame(x=tacsat$SI_LONG,y=tacsat$SI_LATI))
+        spPoint           <- SpatialPoints(data.frame(x=tacsat$SI_LONG[NAS],y=tacsat$SI_LATI[NAS]))
         idx               <- over(spPoint,areas)
-        totres            <- nms[idx]
+        totres[NAS]       <- nms[idx]
 
       } else {
           #Use projection string
           proj4string(lands)<- proj4string
-          spPoint           <- SpatialPoints(data.frame(x=tacsat$SI_LONG,y=tacsat$SI_LATI),proj4string=proj4string)
+          spPoint           <- SpatialPoints(data.frame(x=tacsat$SI_LONG[NAS],y=tacsat$SI_LATI[NAS]),proj4string=proj4string)
           idx               <- over(spPoint,areas)
-          totres            <- nms[idx]
+          totres[NAS]       <- nms[idx]
         }
 return(totres)}
 
