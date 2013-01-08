d8752feae3a676ee71949781ffcdccff7fce1d25
Niels.Hintzen <niels.hintzen@gmail.com>
2013-01-08 14:27:19 +0000

    slight modification to fast option

--- vmstools/R/ICESarea.r
+++ vmstools/R/ICESarea.r
@@ -1,4 +1,4 @@
-ICESarea <- function(tacsat,areas,proj4string=NULL){
+ICESarea <- function(tacsat,areas,proj4string=NULL,fast=FALSE){
       if(class(areas) != "SpatialPolygons") stop("'areas' must be specified as class 'SpatialPolygons'")
 
       #filter NA values
@@ -12,16 +12,17 @@ ICESarea <- function(tacsat,areas,proj4string=NULL){
         if(is.na(proj4string(areas))==FALSE) stop("Projection defined for areas, use proj4string argument in function")
         spPoint           <- SpatialPoints(data.frame(x=tacsat$SI_LONG[NAS], y=tacsat$SI_LATI[NAS]))
         
-        if(nrow(tacsat)>1e4) {
-          cat(paste("too memory demanding when attempting assigning the ICES area in one shoot, then chunk it!\n"))
-           for (chunk in 1: ceiling(nrow(tacsat) / 1e4)) # if number of points exceeds 1e4 then chunk the process to avoid lacking of memory from the over() function!
+        if(nrow(tacsat)>1e4 & fast == FALSE) {
+          idx     <- numeric()
+          cat(paste("Calculation memory demanding, therefore a stepwise approach is taken. use: fast=TRUE for memory demaning approach\n"))
+            for (chunk in 1: ceiling(nrow(tacsat) / 1e4)) # if number of points exceeds 1e4 then chunk the process to avoid lacking of memory from the over() function!
               {
-              cat(paste("chunk...[",(1+(1e4*(chunk-1))),", ", min((1e4*chunk), nrow(tacsat)),"]\n"))
+              #cat(paste("chunk...[",(1+(1e4*(chunk-1))),", ", min((1e4*chunk), nrow(tacsat)),"]\n"))
               idx <- c(idx, over(spPoint [(1+(1e4*(chunk-1))): min((1e4*chunk), nrow(tacsat)),] , areas) )
               }
-        } else{
-         idx               <- over(spPoint, areas)
-        }
+        } else {
+          idx               <- over(spPoint, areas)
+          }
         
         totres[NAS]       <- nms[idx]
 
@@ -29,16 +30,17 @@ ICESarea <- function(tacsat,areas,proj4string=NULL){
           #Use projection string
           proj4string(lands)<- proj4string
           spPoint           <- SpatialPoints(data.frame(x=tacsat$SI_LONG[NAS],y=tacsat$SI_LATI[NAS]), proj4string=proj4string)
-          if(nrow(tacsat)>1e4) {
-          cat(paste("too memory demanding when attempting assigning the ICES area in one shoot, then chunk it!\n"))
-           for (chunk in 1: ceiling(nrow(tacsat) / 1e4)) # if number of points exceeds 1e4 then chunk the process to avoid lacking of memory from the over() function!
+          if(nrow(tacsat)>1e4 & fast == FALSE) {
+            idx     <- numeric()
+            cat(paste("Calculation memory demanding, therefore a stepwise approach is taken. use: fast=TRUE for memory demaning approach\n"))
+            for (chunk in 1: ceiling(nrow(tacsat) / 1e4)) # if number of points exceeds 1e4 then chunk the process to avoid lacking of memory from the over() function!
               {
-              cat(paste("chunk...[",(1+(1e4*(chunk-1))),", ", min((1e4*chunk), nrow(tacsat)),"]\n"))
+              #cat(paste("chunk...[",(1+(1e4*(chunk-1))),", ", min((1e4*chunk), nrow(tacsat)),"]\n"))
               idx <- c(idx, over(spPoint [(1+(1e4*(chunk-1))): min((1e4*chunk), nrow(tacsat)),] , areas) )
               }
-           } else{
-           idx               <- over(spPoint, areas)
-           }
+            } else {
+              idx               <- over(spPoint, areas)
+            }
         
           totres[NAS]       <- nms[idx]
         }
