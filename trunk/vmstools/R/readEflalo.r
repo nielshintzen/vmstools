1a9c6f23f571c09a1a7ecd22add9a62ae9d4232f
fbastardie <fbastardie@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2013-09-13 07:40:08 +0000

    correct for the variable name

--- trunk/vmstools/R/readEflalo.r
+++ trunk/vmstools/R/readEflalo.r
@@ -10,9 +10,9 @@ readEflalo <- function(file,sep=",",dec="."){
                               stop(paste(file,"needs correct header including",paste(c("VE_REF","VE_FLT","VE_COU","VE_LEN","VE_KW","VE_TON","FT_REF","FT_DCOU","FT_DHAR","FT_DDAT","FT_DTIME",
                                   "FT_LCOU","FT_LHAR","FT_LDAT","FT_LTIME","LE_ID","LE_CDAT","LE_STIME","LE_ETIME","LE_SLAT","LE_SLON",
                                   "LE_ELON","LE_ELON","LE_GEAR","LE_MSZ"))))
-                  if(length(grep("KG",colnames(eflalo))) < 1) stop("Units of landings are not in KG")
-                  if(length(grep("LE_KG",colnames(eflalo))) < 1) stop("No landing data provided")
-                  if(length(grep("LE_EURO",colnames(eflalo))) < 1) warning("Currency used is different than 'EURO'")
+                  if(length(grep("KG",colnames(res))) < 1) stop("Units of landings are not in KG")
+                  if(length(grep("LE_KG",colnames(res))) < 1) stop("No landing data provided")
+                  if(length(grep("LE_EURO",colnames(res))) < 1) warning("Currency used is different than 'EURO'")
 
                     #Reformat the data
                   res <- formatEflalo(res)
