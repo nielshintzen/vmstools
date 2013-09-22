295e63d7905d6b858fe7e86b93c2af4d834c6d8d
Niels.Hintzen@gmail.com <Niels.Hintzen@gmail.com@cbb0f966-17ad-11df-bcdc-a7d16bff891f>
2013-09-22 16:10:21 +0000

    Addition of overlapPolygons (calculate area between polygons) Addition of findArea (find smallest area given surface threshold)
    Bug fixes in clipObs2Tacsat and splitAmongPings

--- trunk/vmstools/R/countPings.r
+++ trunk/vmstools/R/countPings.r
@@ -24,11 +24,11 @@ countPings <- function(formula,tacsat,grid=NULL,by=NULL){
                   #Add time notation if you want this as output
                   if(length(timeVars)>0){
                     if(!length(grep("SI_DATIM",colnames(tacsat)))>0) tacsat$SI_DATIM <- as.POSIXct(paste(tacsat$SI_DATE,  tacsat$SI_TIME,   sep=" "), tz="GMT", format="%d/%m/%Y  %H:%M")
-                    if("day" %in% timeVars){       tacsat$SI_DAY   <- an(format(tacsat$SI_DATIM,format="%j"))   ; timeVars[which(timeVars=="day")]      <- "SI_DAY"}
-                    if("week" %in% timeVars){      tacsat$SI_WEEK  <- an(format(tacsat$SI_DATIM,format="%W"))   ; timeVars[which(timeVars=="week")]     <- "SI_WEEK" }
-                    if("month" %in% timeVars){     tacsat$SI_MONTH <- an(format(tacsat$SI_DATIM,format="%m"))   ; timeVars[which(timeVars=="month")]    <- "SI_MONTH"}
-                    if("quarter" %in% timeVars){   tacsat$SI_QUART <- an(substr(quarters(tacsat$SI_DATIM),2,2)) ; timeVars[which(timeVars=="quarter")]  <- "SI_QUART"}
-                    if("year" %in% timeVars){      tacsat$SI_YEAR  <- an(format(tacsat$SI_DATIM,format="%Y"))   ; timeVars[which(timeVars=="year")]     <- "SI_YEAR" }
+                    if("day" %in% timeVars & !"SI_DAY" %in% colnames(tacsat)){       tacsat$SI_DAY   <- an(format(tacsat$SI_DATIM,format="%j"))};      if("day" %in% timeVars){   ; timeVars[which(timeVars=="day")]      <- "SI_DAY"}
+                    if("week" %in% timeVars & !"SI_WEEK" %in% colnames(tacsat)){      tacsat$SI_WEEK  <- an(format(tacsat$SI_DATIM,format="%W"))};     if("week" %in% timeVars){   ; timeVars[which(timeVars=="week")]     <- "SI_WEEK" }
+                    if("month" %in% timeVars & !"SI_MONTH" %in% colnames(tacsat)){     tacsat$SI_MONTH <- an(format(tacsat$SI_DATIM,format="%m"))};    if("month" %in% timeVars){   ; timeVars[which(timeVars=="month")]    <- "SI_MONTH"}
+                    if("quarter" %in% timeVars & !"SI_QUART" %in% colnames(tacsat)){   tacsat$SI_QUART <- an(substr(quarters(tacsat$SI_DATIM),2,2))};  if("quarter" %in% timeVars){ ; timeVars[which(timeVars=="quarter")]  <- "SI_QUART"}
+                    if("year" %in% timeVars & !"SI_YEAR" %in% colnames(tacsat)){      tacsat$SI_YEAR  <- an(format(tacsat$SI_DATIM,format="%Y"))};     if("year" %in% timeVars){   ; timeVars[which(timeVars=="year")]     <- "SI_YEAR" }
                   }
                   #Add spatial notation if you want this as output
                   if(length(spatVars)>0){
@@ -48,8 +48,16 @@ countPings <- function(formula,tacsat,grid=NULL,by=NULL){
                       tacsat$GR_LATI            <- newCoords[,2]
                       spatVars[which(spatVars=="gridcell")] <- "GR_LONG"; spatVars <- c(spatVars,"GR_LATI")
                     }
-                    if("icesrectangle" %in% spatVars){   tacsat$LE_RECT <- ICESrectangle(tacsat); spatVars[which(spatVars=="icesrectangle")] <- "LE_RECT" }
-                    if("icesarea" %in% spatVars){        tacsat$LE_AREA <- ICESarea(tacsat)     ; spatVars[which(spatVars=="icesarea")] <- "LE_AREA"}
+                    if("icesrectangle" %in% spatVars){
+                      if(!"LE_RECT" %in% colnames(tacsat))
+                        tacsat$LE_RECT <- ICESrectangle(tacsat)
+                      spatVars[which(spatVars=="icesrectangle")] <- "LE_RECT"
+                    }
+                    if("icesarea" %in% spatVars){
+                      if(!"LE_AREA" %in% colnames(tacsat))
+                        tacsat$LE_AREA <- ICESarea(tacsat)
+                      spatVars[which(spatVars=="icesarea")] <- "LE_AREA"
+                    }
                   }
 
                   if(is.null(by)){
